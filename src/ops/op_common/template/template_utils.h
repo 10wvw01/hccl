fb378ce646de9572ede96ed0ae45e0c81898b662
baochangjin <baochangjin3@h-partners.com>
2026-04-27 19:23:45 +0800

    scatter omnipipe

--- src/ops/op_common/template/template_utils.h
+++ src/ops/op_common/template/template_utils.h
@@ -245,6 +245,7 @@ struct TemplateDataParams {
     bool enableRemoteMemAccess{false};
     u64 processedDataCount{0};
     u64 root{0};
+    u64 localCopyFlag{0};
     HcclDataType dataType{HCCL_DATA_TYPE_INT8};
     std::vector<u64> allRankSliceSize;
     std::vector<u64> allRankDispls;
@@ -278,6 +279,7 @@ struct TemplateDataParams {
         binaryStream << rdispls;
         binaryStream << allRankProcessedDataCount;
         binaryStream << root;
+        binaryStream << localCopyFlag;
         binaryStream << dataType;
         binaryStream << stepSliceInfo.Serialize();
         binaryStream << opType;
@@ -307,6 +309,7 @@ struct TemplateDataParams {
         binaryStream >> rdispls;
         binaryStream >> allRankProcessedDataCount;
         binaryStream >> root;
+        binaryStream >> localCopyFlag;
         binaryStream >> dataType;
         std::vector<char> stepSliceInfoData;
         binaryStream >> stepSliceInfoData;
