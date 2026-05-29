bc2cbed36434a420cae38b0de096aa2bc08163e1
DengLLy <lideng2@huawei.com>
2026-05-29 10:14:53 +0800

    [refactor] ReduceScatterV: 同步AllGatherV多层级Mesh1D重构 — 扁平化拓扑+删除序列执行器

--- src/ops/reduce_scatter_v/executor/ins_v2_reduce_scatter_v_sole_executor.cc
+++ src/ops/reduce_scatter_v/executor/ins_v2_reduce_scatter_v_sole_executor.cc
@@ -9,6 +9,7 @@
  */
 
 #include "ins_v2_reduce_scatter_v_sole_executor.h"
+#include "topo_match_multilevel_mesh1d.h"
 #include "ins_temp_reduce_scatter_v_mesh_1D.h"
 #if CANN_VERSION_NUM >= CANN_VERSION(9, 0, 0)
 #include "ccu_temp_reduce_scatter_v_mesh_1D_mem2mem.h"
@@ -171,6 +172,10 @@ HcclResult InsV2ReduceScatterVSoleExecutor<AlgTopoMatch, InsAlgTemplate>::Orches
 // 第二个参数是Reduce Scatter的template文件
 REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER_V, InsReduceScatterVMesh1D, InsV2ReduceScatterVSoleExecutor, TopoMatch1D,
     InsTempReduceScatterVMesh1D);
+#if !defined(HCCL_CANN_COMPAT_850)
+REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER_V, InsReduceScatterVMultilevelMesh1D, InsV2ReduceScatterVSoleExecutor,
+    TopoMatchMultilevelMesh1D, InsTempReduceScatterVMesh1D);
+#endif /* !HCCL_CANN_COMPAT_850 */
 #ifndef AICPU_COMPILE
 #if CANN_VERSION_NUM >= CANN_VERSION(9, 0, 0)
 REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_REDUCE_SCATTER_V, CcuReduceScatterVMesh1D, InsV2ReduceScatterVSoleExecutor, TopoMatch1D,
