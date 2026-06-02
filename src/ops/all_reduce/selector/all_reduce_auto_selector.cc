6e5978b5df3157a96b17ce1bf994bfa9964d3348
NANYI00 <hanyan36@huawei.com>
2026-06-02 19:33:17 +0800

    overlap01
    Co-authored-by: NANYI00<hanyan36@huawei.com>
    
    !1153 merge overlap into master
    
    overlap01
    
    Created-by: NANYI00
    Commit-by: NANYI00
    Merged-by: cann-robot
    Description: ## 描述
    针对inplace场景，ccu算法回退aicpu
    
    NA
    
    自验证
    
    NA
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/hccl!1153

--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -38,7 +38,6 @@ SelectorStatus AllReduceAutoSelector::SelectCcuMsAlgo(const TopoInfoWithNetLayer
         HCCL_DEBUG("[AllReduceAutoSelector] levelNum > 1 is not supported yet for ccu_ms mode.");
         return SelectorStatus::NOT_MATCH;
     }
-
     // MS 模式不支持 int8
     CHK_PRT_RET(opParam.DataDes.dataType == HcclDataType::HCCL_DATA_TYPE_INT8,
         HCCL_DEBUG("[AllReduceAutoSelector] dataType[%d] is not supported yet for ccu_ms mode.",
@@ -95,38 +94,36 @@ SelectorStatus AllReduceAutoSelector::SelectMeshUBXAlgo(const TopoInfoWithNetLay
 SelectorStatus AllReduceAutoSelector::SelectMeshAlgo(const TopoInfoWithNetLayerDetails* topoInfo, const OpParam &opParam,
                                                     std::string &selectAlgName) const
 {
-    // u64 perDataSize = DATATYPE_SIZE_TABLE[opParam.DataDes.dataType];
-    // u64 dataSize = opParam.DataDes.count * perDataSize;
-    // if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
-    //     if (IsInputOutputOverlap(opParam) == true) {
-    //         // 不支持 inplace 场景
-    //         return SelectorStatus::NOT_MATCH;
-    //     }
-    //     if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_REGULAR) {
-    //         if(IsSmallData(dataSize)) {
-    //             selectAlgName = "CcuAllReduceMesh2Die"; 
-    //         } else {
-    //             selectAlgName = "CcuAllreduceMesh2DieBigMs"; 
-    //         }
-    //     } else if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_NOT_REGULAR) {
-    //         HCCL_DEBUG("[AllReduceAutoSelector][%s] TWO_DIE_NOT_REGULAR not match", __func__);
-    //         return SelectorStatus::NOT_MATCH;
-    //     } else if (IsSmallData(dataSize)) {
-    //         selectAlgName = "CcuAllReduceMesh1DOneShot";
-    //     } else {
-    //         selectAlgName = "CcuAllReduceMesh1D";
-    //     }
-    // } else if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
-    //     if (IsInputOutputOverlap(opParam) == true) {
-    //         // 不支持 inplace 场景
-    //         return SelectorStatus::NOT_MATCH;
-    //     }
-    //     return SelectMeshUBXAlgo(topoInfo, selectAlgName, dataSize);
-    // } else {
-    //     HCCL_DEBUG("[AllReduceAutoSelector] level0Topo[%u] is not supported yet.", topoInfo->level0Topo);
-    //     return SelectorStatus::NOT_MATCH;
-    // }
-    selectAlgName = "CcuV2AllReduceOmniPipe2D";
+    u64 perDataSize = DATATYPE_SIZE_TABLE[opParam.DataDes.dataType];
+    u64 dataSize = opParam.DataDes.count * perDataSize;
+    if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
+        if (IsInputOutputOverlap(opParam) == true) {// 不支持 inplace 场景
+            return SelectorStatus::NOT_MATCH;
+        }
+        if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_REGULAR) {
+            if(IsSmallData(dataSize)) {
+                selectAlgName = "CcuAllReduceMesh2Die"; 
+            } else {
+                selectAlgName = "CcuAllreduceMesh2DieBigMs"; 
+            }
+        } else if (topoInfo->level0MeshType == Level0MeshType::TWO_DIE_NOT_REGULAR) {
+            HCCL_DEBUG("[AllReduceAutoSelector][%s] TWO_DIE_NOT_REGULAR not match", __func__);
+            return SelectorStatus::NOT_MATCH;
+        } else if (IsSmallData(dataSize)) {
+            selectAlgName = "CcuAllReduceMesh1DOneShot";
+        } else {
+            selectAlgName = "CcuAllReduceMesh1D";
+        }
+    } else if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
+        if (IsInputOutputOverlap(opParam) == true) {
+            // 不支持 inplace 场景
+            return SelectorStatus::NOT_MATCH;
+        }
+        return SelectMeshUBXAlgo(topoInfo, selectAlgName, dataSize);
+    } else {
+        HCCL_DEBUG("[AllReduceAutoSelector] level0Topo[%u] is not supported yet.", topoInfo->level0Topo);
+        return SelectorStatus::NOT_MATCH;
+    }
     HCCL_DEBUG("[AllReduceAutoSelector][%s] Algo match [%s]", __func__, selectAlgName.c_str());
     return SelectorStatus::MATCH;
 }
@@ -163,7 +160,7 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNe
             } else if (topoInfo->is2DieFullMesh) {
                 HCCL_DEBUG("[AllReduceAutoSelector] 2DieFullMesh is not supported yet for ccu schedule mode.");
                 return SelectorStatus::NOT_MATCH;
-            } else if (dataSize <= AR_FLATTEN_MAX_DATA_SIZE && topoInfo->userRankSize <= ccuSize) {
+            } else if (dataSize <= AR_FLATTEN_MAX_DATA_SIZE && topoInfo->userRankSize <= ccuSize && (!IsInputOutputOverlap(opParam))) {
                 selectAlgName = "CcuAllReduceMesh1DMem2Mem";
                 return SelectorStatus::MATCH;
             } else if(IsSmallDataCCU(dataSize, topoInfo->userRankSize)){//64M以下跑ccu
@@ -176,7 +173,7 @@ SelectorStatus AllReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNe
             } else {
                 return SelectorStatus::NOT_MATCH;//64M以上切为aicpu
             }
-        } else if (topoInfo->level0Topo == Level0Shape::CLOS) {
+        } else if (topoInfo->level0Topo == Level0Shape::CLOS &&(!IsInputOutputOverlap(opParam))) {
             selectAlgName = "CcuAllReduceNHR1D";
         } else {
             HCCL_DEBUG("[AllReduceAutoSelector] level0Topo[%d] is not supported yet for ccu schedule mode.",
