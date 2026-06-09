17c794a8fc87aeb94354efd395092705f13e8e1f
DengLLy <lideng2@huawei.com>
2026-06-09 19:52:39 +0800

    fix: 修复3层拓扑测试用例TopoMeta与rankSize不匹配问题
    - all_gather_v: 3layer_2pod_2server 从4rank改为8rank，使用正确的三层拓扑 {{{0,1},{0,1}},{{0,1},{0,1}}}
    - all_gather_v: 新增 3layer_2pod_1server_8rank 测试，覆盖L1退化+多rank场景
    - reduce_scatter_v: 修复3layer TopoMeta {{{0,1},{2,3}},{{0,1},{2,3}}} -> {{{0},{1}},{{0},{1}}}

--- test/st/algorithm/testcase/reduce_scatter_v_testcase.cc
+++ test/st/algorithm/testcase/reduce_scatter_v_testcase.cc
@@ -85,7 +85,7 @@ TEST_F(ST_REDUCESCATTERV_TEST, st_reduce_scatter_v_a5_multilevel_2pod_4rank_int3
 
 TEST_F(ST_REDUCESCATTERV_TEST, st_reduce_scatter_v_a5_3layer_2pod_2server_4rank_fp16_equal_test)
 {
-    TopoMeta topoMeta{{{0, 1}, {2, 3}}, {{0, 1}, {2, 3}}};
+    TopoMeta topoMeta{{{0}, {1}}, {{0}, {1}}};
     VDataDesTag vDataDes;
     vDataDes.counts = {200, 200, 200, 200};
     vDataDes.displs = {0, 200, 400, 600};
