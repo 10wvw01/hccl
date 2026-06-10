7a73fa00547e84da3c142f7dc91a7ea5d604ff53
DengLLy <lideng2@huawei.com>
2026-06-10 16:58:47 +0800

    Remove level0Topo check in AICPU selector for AllGatherV and ReduceScatterV

--- src/ops/reduce_scatter_v/selector/reduce_scatter_v_auto_selector.cc
+++ src/ops/reduce_scatter_v/selector/reduce_scatter_v_auto_selector.cc
@@ -175,12 +175,9 @@ SelectorStatus ReduceScatterVAutoSelector::SelectAicpuAlgo(const TopoInfoWithNet
         return SelectorStatus::NOT_MATCH;
     }
 
-    if (topoInfo->topoLevelNums >= 1 && topoInfo->topoLevelNums <= 3 &&
-        topoInfo->level0Topo == Level0Shape::MESH_1D) {
+    if (topoInfo->topoLevelNums >= 1 && topoInfo->topoLevelNums <= 3) {
         selectAlgName = "InsReduceScatterVMesh1D";
     } else {
-        HCCL_WARNING("[ReduceScatterVAutoSelector] topo not match, topoLevelNums[%u], level0Topo[%u]",
-            topoInfo->topoLevelNums, static_cast<u32>(topoInfo->level0Topo));
         return SelectorStatus::NOT_MATCH;
     }
     return SelectorStatus::MATCH;
