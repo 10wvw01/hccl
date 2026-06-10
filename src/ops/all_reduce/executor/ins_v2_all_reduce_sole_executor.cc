7ec6809def4a1410758708da226f3eda02d1209a
wjl <jellywang2000@gmail.com>
2026-06-10 17:53:06 +0800

    fast launch

--- src/ops/all_reduce/executor/ins_v2_all_reduce_sole_executor.cc
+++ src/ops/all_reduce/executor/ins_v2_all_reduce_sole_executor.cc
@@ -218,7 +218,11 @@ HcclResult InsV2AllReduceSoleExecutor<AlgTopoMatch, InsAlgTemplate>::FastLaunchS
     // 3 ccu kernel handle, taskArg入参
     ccuFastLaunchCtx->ccuKernelNum[0] = ccuKernelNum;
     CcuKernelSubmitInfo *kernelSubmitInfos = ccuFastLaunchCtx->GetCcuKernelSubmitInfoPtr();
+<<<<<<< HEAD
     for (int i = 0; i < ccuKernelNum; i++) {
+=======
+    for (u32 i = 0; i < ccuKernelNum; i++) {
+>>>>>>> f99b904 (fast launch)
         kernelSubmitInfos[i] = templateAlgRes.submitInfos[i];
     }
     return HCCL_SUCCESS;
