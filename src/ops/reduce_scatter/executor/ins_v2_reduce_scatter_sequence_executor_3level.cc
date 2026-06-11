190c0e877cacb91344b971a3450815ede9fe3651
ssstars <cuiruxing@huawei.com>
2026-06-11 16:37:12 +0800

    fix log format

--- src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_3level.cc
+++ src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_3level.cc
@@ -156,9 +156,9 @@ HcclResult InsV2ReduceScatterSequenceExecutor3Level<AlgTopoMatch, InsAlgTemplate
             myRank_);
     }
     CHK_RET(RestoreChannelMap(resCtx, remoteRankToChannelInfo_));
-    HCCL_INFO("[InsV2ReduceScatterSequenceExecutor3Level] [Orchestrate] myRank_[%llu] rankIdxLevel0_[%llu] "
-        "rankIdxLevel1_[%llu] rankIdxLevel2_[%llu] rankSizeLevel0_[%llu] rankSizeLevel1_[%llu] "
-        "rankSizeLevel2_[%llu]",
+    HCCL_INFO("[InsV2ReduceScatterSequenceExecutor3Level] [Orchestrate] myRank_[%lu] rankIdxLevel0_[%lu] "
+        "rankIdxLevel1_[%lu] rankIdxLevel2_[%lu] rankSizeLevel0_[%lu] rankSizeLevel1_[%lu] "
+        "rankSizeLevel2_[%lu]",
         myRank_, rankIdxLevel0_, rankIdxLevel1_,rankIdxLevel2_, rankSizeLevel0_, rankSizeLevel1_, rankSizeLevel2_);
     HcclResult ret = OrchestrateLoop(param, resCtx);
     CHK_PRT_RET(ret != HCCL_SUCCESS,
