42542a9b13c415d8575a563d7be310e013a9271a
ssstars <cuiruxing@huawei.com>
2026-06-11 17:35:49 +0800

    fix: use %u format for uint32_t rankIdx/rankSize in 3level executor

--- src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_3level.cc
+++ src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_3level.cc
@@ -163,7 +163,10 @@ HcclResult InsV2ReduceScatterSequenceExecutor3Level<AlgTopoMatch, InsAlgTemplate
             myRank_);
     }
     CHK_RET(RestoreChannelMap(resCtx, remoteRankToChannelInfo_));
-
+    HCCL_INFO("[InsV2ReduceScatterSequenceExecutor3Level] [Orchestrate] myRank_[%u] rankIdxLevel0_[%u] "
+        "rankIdxLevel1_[%u] rankIdxLevel2_[%u] rankSizeLevel0_[%u] rankSizeLevel1_[%u] "
+        "rankSizeLevel2_[%u]",
+        myRank_, rankIdxLevel0_, rankIdxLevel1_,rankIdxLevel2_, rankSizeLevel0_, rankSizeLevel1_, rankSizeLevel2_);
     HcclResult ret = OrchestrateLoop(param, resCtx);
     CHK_PRT_RET(ret != HCCL_SUCCESS,
         HCCL_ERROR("[InsV2ReduceScatterSequenceExecutor3Level][Orchestrate] errNo[0x%016llx] "
