81907ebab6f9d202974d2645bc30acff36a67c1b
xiaocui235 <cuiruxing@huawei.com>
2026-06-11 12:09:31 +0800

    feat: sync PR1158 rebased to 2026-06-05

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.cc
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.cc
@@ -139,6 +139,7 @@ HcclResult CcuTempAllGatherNHR1DMem2Mem::CalcRes(HcclComm comm, const OpParam& p
         kernelArg->subCommRanks = subCommRanks_;
         kernelArg->axisSize = dieNum;
         kernelInfo.setKernelArg(kernelArg);
+
         kernelInfo.channels = channelsPerDie[kernelIdx];
         resourceRequest.ccuKernelInfos.push_back(kernelInfo);
     }
