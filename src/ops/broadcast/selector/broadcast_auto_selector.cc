d339469687172a9e62bcf66fb5d3288f05437f00
xiaocui235 <cuiruxing@huawei.com>
2026-06-11 11:13:35 +0800

    feat: add PR#1272 unique changes on top of PR#1158 base
    - Add TopoMatch3Level and TopoMatchSqueeze2D topo matchers
    - Add OmniPipe executors (AllGather/AllReduce/ReduceScatter)
    - Register Squeeze2D-based parallel algorithms (Broadcast/Reduce/RS)
    - Add 3-level broadcast selector logic
    - Fix EndpointDesc memory management (std::make_unique)
    - Minor fixes: recv_op/send_op null checks, broadcast_op.h signature
    - Add topo CMakeLists entries for new matchers

--- src/ops/broadcast/selector/broadcast_auto_selector.cc
+++ src/ops/broadcast/selector/broadcast_auto_selector.cc
@@ -123,7 +123,13 @@ SelectorStatus BroadcastAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayer
     HCCL_DEBUG("[BroadcastAutoSelector][%s] start, topoInfo levelNum[%u]", __func__, topoInfo->topoLevelNums);
     
     if (topoInfo->topoLevelNums > 1) {
-        if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[0] == 1) {
+        if (topoInfo->topoLevelNums == 3) {
+            if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[1] == 1) {
+                selectAlgName = "InsBroadcastNHR";
+            } else {
+                selectAlgName = "InsBroadcastParallelNHRNHRUboe";
+            }
+        } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[0] == 1) {
             selectAlgName = "InsBroadcastNHR";
         } else if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
             selectAlgName = "InsBroadcastParallelMesh1DNHR";
