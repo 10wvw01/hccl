f82604695d02249b315a49dc4a2c83379673b0f7
NANYI00 <hanyan36@huawei.com>
2026-06-12 16:01:37 +0800

    pro_solve_conflict

--- src/ops/all_gather_v/selector/all_gather_v_auto_selector.cc
+++ src/ops/all_gather_v/selector/all_gather_v_auto_selector.cc
@@ -58,7 +58,8 @@ SelectorStatus AllGatherVAutoSelector::SelectAicpuAlgo(
     HCCL_INFO("hccl algo op config: config opType:%d, level0:%u, level1:%u, level2:%u, level3:%u", opParam.opType,
               algos[0], algos[1], algos[2], algos[3]);
  
-    if (topoInfo->topoLevelNums >= 1 && topoInfo->topoLevelNums <= 3) {
+    if (topoInfo->topoLevelNums >= 1 && topoInfo->topoLevelNums <= 3 &&
+        topoInfo->level0Topo == Level0Shape::MESH_1D) {
         selectAlgName = "InsAllGatherVMesh1D";
     } else {
         HCCL_WARNING("[AllGatherVAutoSelector] topo not match, topoLevelNums[%u], level0Topo[%u]",
