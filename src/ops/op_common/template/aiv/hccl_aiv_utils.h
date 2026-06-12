5075aa3d7f303dca5f541b103e621acc1dec2019
GoodTimeLZ <linzhen9@huawei.com>
2026-06-12 15:51:03 +0800

    1

--- src/ops/op_common/template/aiv/hccl_aiv_utils.h
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.h
@@ -19,7 +19,7 @@
 #include "alg_param.h"
 
 namespace ops_hccl {
-constexpr u32 MAX_RANK_SIZE = 128; // 注意要和device侧的一致
+constexpr u32 MAX_RANK_SIZE = 512; // 注意要和device侧的一致
  
 constexpr s32 TOPO_LEN = 128;
 
