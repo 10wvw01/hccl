80f75ef9e2c2e998fac50d498f565c39754e1591
shenjiahui_ <shenjiahui5@huawei.com>
2026-06-15 15:30:55 +0800

    fix check_ret

--- src/ops/op_common/op_common.cc
+++ src/ops/op_common/op_common.cc
@@ -1212,7 +1212,7 @@ HcclResult HcclGetThread(
         u32 threadNum = resRequest.slaveThreadNum + 1;
         std::vector<ThreadHandle> threads(threadNum);
         std::vector<ThreadConfig> threadConfigs(threadNum);
-        CHK_RET(ThreadConfigInit(threadConfigs.data(), threadNum));
+        CHK_RET(static_cast<HcclResult>(ThreadConfigInit(threadConfigs.data(), threadNum)));
         threadConfigs[0].notifyNumPerThread = resRequest.notifyNumOnMainThread + 1; // 主流上多一个用于host-device同步
         for (u32 i = 1; i < threadNum; i++) {
             threadConfigs[i].notifyNumPerThread = resRequest.notifyNumPerThread[i];
@@ -1223,7 +1223,7 @@ HcclResult HcclGetThread(
 
         // 申请展开流对应的Thread
         ThreadConfig unfoldThreadConfig;
-        CHK_RET(ThreadConfigInit(&unfoldThreadConfig, 1));
+        CHK_RET(static_cast<HcclResult>(ThreadConfigInit(&unfoldThreadConfig, 1)));
         unfoldThreadConfig.notifyNumPerThread = 0;
         CHK_RET(HcclThreadAcquireWithConfig(comm, COMM_ENGINE_CPU, 1, THREAD_TYPE_TS,
             &unfoldThreadConfig, &resCtxHost->unfoldThread));
@@ -1259,7 +1259,7 @@ HcclResult GeGetThread(HcclComm comm, const OpParam &param, AlgResourceRequest &
         if (threadNum > 0) {
             std::vector<ThreadHandle> threads(threadNum);
             std::vector<ThreadConfig> threadConfigs(threadNum);
-            CHK_RET(ThreadConfigInit(threadConfigs.data(), threadNum));
+            CHK_RET(static_cast<HcclResult>(ThreadConfigInit(threadConfigs.data(), threadNum)));
             for (u32 i = 0; i < threadNum; i++) {
                 threadConfigs[i].notifyNumPerThread = resRequest.notifyNumPerThread[i];
             }
