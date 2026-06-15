912ea824804c41c5dbccc9c6e2192b2c9f91a91b
chenyia <chenyi163@huawei.com>
2026-06-15 22:59:53 +0800

    alltoallv ccu

--- src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.cc
+++ src/ops/all_to_all_v/executor/ins_v2_all_to_all_v_sole_executor.cc
@@ -19,6 +19,7 @@
 #include "ccu_temp_all_to_all_mesh2die.h"
 #include "ccu_temp_all_to_all_mesh1d_multi_jetty.h"
 #include "ccu_temp_all_to_all_mesh1d_2Die.h"
+#include "ccu_temp_all_to_all_v_mesh1d_2Die.h"
 #endif /* !HCCL_CANN_COMPAT_850 */
 #endif
 
@@ -469,6 +470,8 @@ REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLTOALLVC, InsAlltoAllVCClosMesh1DDPU, I
         CcuTempAlltoAllMesh1D);
     REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLTOALL, CcuAllToAllMesh1D2Die, InsV2AlltoAllVSoleExecutor, TopoMatch1D,
         CcuTempAllToAllMesh1D2Die);
+    REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLTOALLV, CcuAllToAllVMesh1D2Die, InsV2AlltoAllVSoleExecutor, TopoMatch1D,
+        CcuTempAllToAllVMesh1D2Die);
 #endif /* !HCCL_CANN_COMPAT_850 */
     REGISTER_EXEC_V2(HcclCMDType::HCCL_CMD_ALLTOALL, AivAlltoAllMesh1D, InsV2AlltoAllVSoleExecutor, TopoMatch1D,
                      AivTempAlltoAllMesh1D);
---
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_v_mesh1d_2Die.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef HCCLV2_CCU_TEMP_ALL_TO_ALL_V_MESH_1D_2DIE_H_
+#define HCCLV2_CCU_TEMP_ALL_TO_ALL_V_MESH_1D_2DIE_H_
+
+#include "utils.h"
+#include "ccu_alg_template_base.h"
+
+namespace ops_hccl {
+
+using RankId = u32;
+using RankGroup = std::vector<RankId>;
+
+class CcuTempAllToAllVMesh1D2Die : public CcuAlgTemplateBase {
+public:
+    CcuTempAllToAllVMesh1D2Die() = default;
+    explicit CcuTempAllToAllVMesh1D2Die(const OpParam &param, RankId rankId,
+        const std::vector<std::vector<u32>> &subCommRanks);
+    ~CcuTempAllToAllVMesh1D2Die() override;
+
+    std::string Describe() const override
+    {
+        return StringFormat("Template of alltoallv ccu mesh 1D 2Die with rankSize[%u]", templateRankSize_);
+    }
+
+    HcclResult CalcRes(HcclComm comm, const OpParam &param, const TopoInfoWithNetLayerDetails *topoInfo,
+        AlgResourceRequest &resourceRequest) override;
+
+    HcclResult KernelRun(const OpParam &param, const TemplateDataParams &templateDataParams,
+        TemplateResource& templateResource) override;
+
+    void SetA2ASendRecvInfo(const A2ASendRecvInfo &sendRecvInfo);
+
+private:
+    HcclResult PartitionChannels(HcclComm comm, const std::vector<HcclChannelDesc> &channelDescs, uint32_t &meshDieId,
+                                std::map<u32, std::vector<HcclChannelDesc>>& rankIdToChannelDesc);
+    void FillRankGroupTaskArgs(uint32_t dieId, const LoopGroupConfig &config, std::vector<uint64_t> &taskArgs);
+    // void FillRankGroupInfo();
+
+    const uint32_t DIE_NUM = 2;
+
+    std::map<uint32_t, std::vector<HcclChannelDesc>> channels_;
+    std::map<uint32_t, RankGroup> rankGroup_;
+    std::map<u32, std::vector<HcclChannelDesc>> rankIdToChannelDesc_;
+
+    A2ASendRecvInfo localSendRecvInfo_;
+};
+
+} // namespace ops_hccl
+#endif // HCCLV2_CCU_TEMP_ALL_TO_ALL_V_MESH_1D_2DIE_H_
