d79a2e2981488b583d76a7626f705a71b3b0e283
unknown <zhangqian305@h-partners.com>
2026-06-16 16:10:21 +0800

    reduce

--- src/ops/reduce/executor/ccu_v2_reduce_omnipipe_executor.cc
+++ src/ops/reduce/executor/ccu_v2_reduce_omnipipe_executor.cc
@@ -853,7 +853,7 @@ REGISTER_EXEC_V2_MULTI(HcclCMDType::HCCL_CMD_REDUCE,
                                 CcuTempReduceScatterOmniPipeMesh1DMem2Mem, 
                                 CcuTempReduceScatterOmniPipeNHR1DMem2Mem, 
                                 CcuTempGatherOmniPipeMesh1DMem2Mem,
-                                // CcuTempGatherOmniPipeMesh1DMem2MemY);
-                                CcuTempGatherOmniPipeNHR1DMem2Mem);
+                                CcuTempGatherOmniPipeMesh1DMem2MemY);
+                                // CcuTempGatherOmniPipeNHR1DMem2Mem);
 // #endif
 }
