2dfa2a45f8e42b01fdd1a8ab3c6d22cfe4827864
GoodTimeLZ <linzhen9@huawei.com>
2026-06-17 16:34:56 +0800

    1

--- src/ops/op_common/template/aiv/aiv_communication_base_v2.h
+++ src/ops/op_common/template/aiv/aiv_communication_base_v2.h
@@ -251,8 +251,6 @@ public:
         countOffset = DOUBLE * pingpongOffset;
         seperateOffset = countOffset + NUM_BLOCKS_FOUR_PER_RANK_A3 * rankSize_ * FLAG_SIZE;
 
-        InitBuffArray(args->buffersIn, pingpong);
-
         pipe.InitBuffer(localFlagBuf, LOCAL_FLAG_BUF_LEN);
         localSetTensor = localFlagBuf.GetWithOffset<int32_t>(UB_FLAG_PAD_COUNT, FLAG_ONE_OFFSET);
         localCheckTensor = localFlagBuf.GetWithOffset<int32_t>(UB_FLAG_PAD_COUNT, FLAG_TWO_OFFSET);
@@ -270,6 +268,7 @@ public:
             ClearSyncBuf();
         }
         GetTag(args->buffersIn);
+        InitBuffArray(args->buffersIn, pingpong);
     }
 
     __aicore__ inline void InitBuffArray(GM_ADDR buffIn, bool pingpong = false)
