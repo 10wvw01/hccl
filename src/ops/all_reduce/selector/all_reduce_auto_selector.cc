bd6043c8240dcbd25e8bf35b5f03fa0377d9b9d2
DengLLy <lideng2@huawei.com>
2026-06-17 15:57:49 +0800

    feat: add level2Uboe check in all_reduce selector for topoLevelNums==3
    - Add level2Uboe field to TopoInfoWithNetLayerDetails struct
    - Add CalcLevel2Uboe to detect UBOE protocol on level2 links
    - Guard Uboe algo selection with level2Uboe check in SelectAicpuAlgo

--- src/ops/all_reduce/selector/all_reduce_auto_selector.cc
+++ src/ops/all_reduce/selector/all_reduce_auto_selector.cc
@@ -337,6 +337,10 @@ SelectorStatus AllReduceAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayer
 
     if (topoInfo->topoLevelNums > 1) {
         if (topoInfo->topoLevelNums == 3) {
+            if (!topoInfo->level2Uboe) {
+                HCCL_DEBUG("[AllReduceAutoSelector] level2 protocol is not UBOE, skip Uboe algos.");
+                return SelectorStatus::NOT_MATCH;
+            }
             if (topoInfo->deviceNumPerModule == 8) {
                 selectAlgName = "InsV2AllReduceOmniPipeUboe";
             } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[1] == 1) {
@@ -362,9 +366,11 @@ SelectorStatus AllReduceAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayer
                 } else {
                     selectAlgName = "InsAllReduceNHR";
                 }
+            } else if (dataSize > AR_AICPU_1D_CROSS_SMALL_DATA_SIZE) {
+                selectAlgName = (dataSize > AR_AICPU_SEQUENCE_DATA_SIZE) ?
+                            "InsAllReduceSequenceMesh1DNhr" : "InsAllReduceParallelRSAG";
             } else {
-                HCCL_ERROR("[all reduce do not support topoInfo->topoLevelNums > 3, but got [%u]", topoInfo->topoLevelNums);
-                return SelectorStatus::NOT_MATCH;
+                selectAlgName = "InsAllReduceNHR";
             }
 
         } else if (topoInfo->level0Topo == Level0Shape::CLOS) {
