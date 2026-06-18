f4615e27bd35e9a1d7ff0ae3e36ba9c9fb6a3faf
unknown <zhangqian305@h-partners.com>
2026-06-18 10:18:08 +0800

    reduce

--- src/ops/op_common/template/template_utils.h
+++ src/ops/op_common/template/template_utils.h
@@ -254,6 +254,7 @@ struct TemplateDataParams {
     std::vector<u64> rdispls;
     StepSliceInfo stepSliceInfo;
     BatchSendRecvOpType opType;
+    u64 localCopyFlag = 0;
 
     std::vector<char> Serialize() const
     {
