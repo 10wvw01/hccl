bef7f9d49a8fbba26dd21cfc036669eb6a64ddd8
zhajianqing123 <zhajianqing123@noreply.gitcode.com>
2026-06-21 16:06:27 +0800

    add broadcast omnipipe

--- src/ops/op_common/template/template_utils.h
+++ src/ops/op_common/template/template_utils.h
@@ -245,6 +245,7 @@ struct TemplateDataParams {
     bool enableRemoteMemAccess{false};
     u64 processedDataCount{0};
     u64 root{0};
+    u64 localCopyFlag{0};
     HcclDataType dataType{HCCL_DATA_TYPE_INT8};
     std::vector<u64> allRankSliceSize;
     std::vector<u64> allRankDispls;
@@ -280,6 +281,7 @@ struct TemplateDataParams {
         binaryStream << rdispls;
         binaryStream << allRankProcessedDataCount;
         binaryStream << root;
+        binaryStream << localCopyFlag;
         binaryStream << dataType;
         binaryStream << stepSliceInfo.Serialize();
         binaryStream << opType;
@@ -311,6 +313,7 @@ struct TemplateDataParams {
         binaryStream >> rdispls;
         binaryStream >> allRankProcessedDataCount;
         binaryStream >> root;
+        binaryStream >> localCopyFlag;
         binaryStream >> dataType;
         std::vector<char> stepSliceInfoData;
         binaryStream >> stepSliceInfoData;
