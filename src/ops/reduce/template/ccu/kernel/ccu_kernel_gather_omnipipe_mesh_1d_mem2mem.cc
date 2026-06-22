f3c7e6c787abd0bc8f40e78d69080168b6a5f171
unknown <zhangqian305@h-partners.com>
2026-06-22 14:18:56 +0800

    reduce

--- src/ops/reduce/template/ccu/kernel/ccu_kernel_gather_omnipipe_mesh_1d_mem2mem.cc
+++ src/ops/reduce/template/ccu/kernel/ccu_kernel_gather_omnipipe_mesh_1d_mem2mem.cc
@@ -163,7 +163,7 @@ CcuResult CcuGatherOmniPipeMesh1DMem2MemKernel(CcuKernelArg arg)
     HCCL_INFO("[CcuGatherOmniPipeMesh1DMem2Mem] GatherOmniPipeMesh1DMem2Mem run");
     CCU_CHK_RET(ParseKernelArg(ctx, kernelArg));
     CCU_CHK_RET(InitResource(ctx));
-    CCU_CHK_RET(LoadArgs(ctx));
+    // CCU_CHK_RET(LoadArgs(ctx));
     
     // CCU_CHK_RET(PreSync(ctx));
     
