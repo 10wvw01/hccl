3bf07aad3cf08aceb52c9755c2f1efcd79dc87d6
chenyia <chenyi163@huawei.com>
2026-06-23 15:29:26 +0800

    fastlaunch

--- src/ops/all_to_all_v/selector/alltoallv_auto_selector.cc
+++ src/ops/all_to_all_v/selector/alltoallv_auto_selector.cc
@@ -27,14 +27,14 @@ SelectorStatus AlltoAllVAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNe
     HCCL_DEBUG("[AlltoAllVAutoSelector][%s] start, topoInfo levelNum[%u]", __func__, topoInfo->topoLevelNums);
     (void)opParam;
     (void)configAlgMap;
-    uint32_t userRankSizeMax = 128;
+    uint32_t userRankSizeMax = 64;
     if (topoInfo->topoLevelNums > 1) {
         if (opParam.all2AllDataDes.sendType == HcclDataType::HCCL_DATA_TYPE_INT8) {
             HCCL_WARNING("[Algo][AlltoAllVAutoSelector] int8 is not supported yet for ccu_schedule mode.");
             return SelectorStatus::NOT_MATCH;
         }
         if (topoInfo->userRankSize > userRankSizeMax) {
-            HCCL_WARNING("[Algo][AlltoAllVAutoSelector] rankSize > 128 is not supported yet for ccu_schedule mode.");
+            HCCL_WARNING("[Algo][AlltoAllVAutoSelector] rankSize > 64 is not supported yet for ccu_schedule mode.");
             return SelectorStatus::NOT_MATCH;
         }
         selectAlgName = "CcuAlltoAllVMesh1D2Die";
