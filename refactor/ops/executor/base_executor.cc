1083605bb7a1af6510657db269dcd4ffa2a22e40
silencesirius <caixin21@huawei.com>
2026-06-25 11:20:07 +0800

    refact base executor

--- refactor/ops/executor/base_executor.cc
+++ refactor/ops/executor/base_executor.cc
@@ -1,23 +1,110 @@
-BaseExecutor {
-    protected AlgHierarchyInfoForAllLevel algHierarchyInfo
+#include "base_executor.h"
 
-    CalcAlgHierarchyInfo(
-        HcclComm comm, TopoInfoWithNetLayerDetails *topoInfo, AlgHierarchyInfoForAllLevel &algHierarchyInfo, AlgTopoMatch topoMatch)
+BaseExecutor::BaseExecutor(HcclAlgorithm &algo)
+    : algo_(algo) {}
+
+BaseExecutor::BaseExecutor(HcclAlgorithm &algo, BaseExecutorParam &param)
+    : algo_(algo), myRank_(param.myRank), rankSize_(param.rankSize),
+      dataType_(param.dataType), dataTypeSize_(param.dataTypeSize), dataCount_(param.dataCount), dataSize_(param.dataSize)
+      reduceOp_(param.reduceOp), root_(param.root) {}
+
+BaseExecutor::~BaseExecutor() {}
+
+// HcclResult BaseExecutor::CalcAlgHierarchyInfo(HcclComm comm, TopoInfoWithNetLayerDetails *topoInfo,
+//     AlgHierarchyInfoForAllLevel &algHierarchyInfo, AlgTopoMatch topoMatch)
+// {
+//     CHK_RET(topoMatch.MatchTopo(comm, topoInfo, algHierarchyInfo));
+//     return HCCL_SUCCESS;
+// }
+
+// TODO：其实叫SplitSubComm更贴切，subComm直接就是每个子通信域的集合
+
+using CommInfo = std::vector<u32>;
+using CommInfoList = std::vector<CommInfo>;
+
+HcclResult BaseExecutor::SplitSubComm(HcclComm comm, TopoInfoWithNetLayerDetails *topoInfo,
+    AlgTopoMatch topoMatch, CommInfoList &CommInfoList)
+{
+    CHK_RET(topoMatch.MatchTopo(comm, topoInfo, CommInfoList));
+    return HCCL_SUCCESS;
+}
+
+HcclResult BaseExecutor::Plan(const CommInfoList &commInfoList, std::vector<RankPair> &rankPairList)
+{
+    // 遍历每个子通信域，计算建链关系
+    for (auto i = 0; i < commInfoList.size(); ++i) {
+        CalcChannelReq(commInfoList.at(i), algo_.templates.at(i).algType, rankPairList);
+    }
+    // TODO：这里考虑是直接push_back到rankPairList最后，还是每个计算完merge一把（可能要去重）
+}
+
+HcclResult CalcRes(const CommInfoList &CommInfoList, AlgResourceRequest &resReq)
+{
+    std::vector<AlgResourceRequest> resReqList;
+    for (auto i = 0; i < algo_.templates.size(); ++i) {
+        // 实例化template
+        u32 subRankSize = CommInfoList.at(i).size();
+        auto singleTemplate = GenTemplate(subRankSize);
+        // 计算每个template资源
+        AlgResourceRequest resReqTmp;
+        singleTemplate.CalcRes(algo_.opType, resReqTmp);
+        resReqList.push_back(resReqTmp);
+    }
+
+    // TODO:合并每个实例的template资源，不同的Executer合并方式不同
+    MergeResReq(resReqList, resReq);
+}
+
+HcclResult Orchestrate(const BaseExecutorParam &baseExecutorParam, ConfigParam &configParam,
+    const BufferParam &bufferParam, const CommInfoList &CommInfoList)
+{
+    // 切分资源给每个Template
+    SplitRes();
+    // 切分数据循环
+    SplitDataLoop();
+    // 循环展开
+    OrchestrateLoop();
+    // TODO：储存队列和任务信息，用于FastLauch
+    SaveCtx();
+}
+
+// private
+
+HcclResult CalcChannelReq(const CommInfo &commInfo, const AlgoType algoType, std::vector<RankPair> &rankPairList)
+{
+    // commInfo里就是一个rank列表
+    // 根据commInfo和algoType足以计算建链关系
+}
+
+HcclResult MergeResReq(std::vector<AlgResourceRequest> &resReqList, AlgResourceRequest &resReq)
+{
+    // 不同Executor整合资源的形式不同，有的取最大，有的加起来
+    switch (algo_.executorType)
     {
-        CHK_RET(topoMatch.MatchTopo(comm, topoInfo, algHierarchyInfo));
-        return HCCL_SUCCESS;
+    case ExecutorType::SOLE:
+        // TODO
+        break;
+    case ExecutorType::PARALLEL:
+        // TODO
+        break;
+    default:
+        break;
     }
+}
 
-    Plan(AlgHierarchyInfoForAllLevel &algHierarchyInfo, HcclAlgorithm &alg) {
-        template0.plan()
-        template1.plan()
+HcclResult SplitRes()
+{
+    // 不同Executor切分资源给每个template，Merge的逆向操作
+    // 取代PrepareResForTemplate函数
+    switch (algo_.executorType)
+    {
+    case ExecutorType::SOLE:
+        // TODO
+        break;
+    case ExecutorType::PARALLEL:
+        // TODO
+        break;
+    default:
+        break;
     }
-
-    virtual CalcRes(AlgHierarchyInfoForAllLevel &algHierarchyInfo);
-
-    virtual Orchestrate();
-
-    //TODO:
-    FastLaunch()
-    
 }
---
+++ refactor/ops/executor/base_executor.h
@@ -0,0 +1,84 @@
+#include "hccl_algorithm.h"
+
+class BaseExecutor {
+public:
+    BaseExecutor(HcclAlgorithm &algo);
+    // TODO：是否需要2种构造函数，一种用于CalcRes，另一种用于Orchestrate
+    BaseExecutor(HcclAlgorithm &algo, BaseExecutorParam &param);
+    ~BaseExecutor();
+
+protected:
+    // Describe函数完全没用到，可以删除
+    // std::string Describe();
+
+    HcclResult CalcAlgHierarchyInfo(HcclComm comm, TopoInfoWithNetLayerDetails *topoInfo,
+        AlgHierarchyInfoForAllLevel &algHierarchyInfo, AlgTopoMatch topoMatch);
+
+    HcclResult Plan(AlgHierarchyInfoForAllLevel &algHierarchyInfo);
+
+    virtual HcclResult CalcRes(AlgHierarchyInfoForAllLevel &algHierarchyInfo);
+
+    virtual HcclResult Orchestrate(const BaseExecutorParam &baseExecutorParam, ConfigParam &configParam,
+        const BufferParam &bufferParam, const CommInfoList &CommInfoList);
+
+    //TODO: 目前仅用于CCU，理论上可扩展至所有模式
+    HcclResult FastLaunch();
+
+    // algo
+    HcclAlgorithm algo_;
+
+    // rankInfo
+    u32 myRank_ = INVALID_VALUE_RANKID;
+    u32 rankSize_ = 0;
+
+    // dataInfo
+    HcclDataType dataType_;
+    u64 dataTypeSize_ = 0;
+    u64 dataCount_ = 0;
+    u64 dataSize_ = 0;
+
+    // opInfo
+    HcclReduceOp reduceOp_;
+    u32 root_ = INVALID_VALUE_RANKID;
+    
+    // TODO：分析下使用场合
+    DevType devType_ = DevType::DEV_TYPE_COUNT;
+};
+
+
+struct BaseExecutorParam {
+    u32 myRank = INVALID_VALUE_RANKID;
+    u32 rankSize = 0;
+
+    HcclDataType dataType = HCCL_DATA_TYPE_RESERVED;
+    u64 dataTypeSize = 0;
+    u64 dataCount = 0;
+    u64 dataSize = 0;
+
+    HcclReduceOp reduceOp = HCCL_REDUCE_RESERVED;  // reduce类型，搬运类算子使用默认值
+    u32 root = INVALID_VALUE_RANKID;  // root节点所在rank，不涉及root算子使用默认值
+
+    // TODO：针对带V的算子，需要额外传入数组
+    u8 varData = 0;
+};
+
+struct ConfigParam {
+    OpMode opMode;
+    // TODO：绕路参数
+};
+
+struct BufferParam {
+    Buffer inputBuffer;
+    Buffer outputBuffer;
+    Buffer cclBuffer;
+
+    // cclBuffer统一用ptr和size来描述，template里面也是用这些参数，不使用HcclMem
+    // 删除冗余maxTmpMemSize_参数
+};
+
+struct Buffer {
+    void* ptr;
+    u64 size;
+    BufferType bufferType;
+};
+
