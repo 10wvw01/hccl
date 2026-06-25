5781ebd86223c5cf8a151fed52f3ce74434ba8ba
yjzz1007 <zhaojiayu7@huawei.com>
2026-06-25 11:36:05 +0800

    bugfix

--- src/ops/all_gather/executor/ins_v2_all_gather_parallel_executor.cc
+++ src/ops/all_gather/executor/ins_v2_all_gather_parallel_executor.cc
@@ -622,7 +622,7 @@ REGISTER_EXECUTOR_BY_TWO_TEMPS(HcclCMDType::HCCL_CMD_ALLGATHER, CcuAllGatherPara
 
 #if CANN_VERSION_NUM >= CANN_VERSION(9, 0, 0)
 REGISTER_EXECUTOR_BY_TWO_TEMPS(HcclCMDType::HCCL_CMD_ALLGATHER, CcuAllGatherParallelMesh1DNHRMemMultiJetty,
-    InsV2AllGatherParallelExecutor, TopoMatchUBX, CcuTempAllGatherMesh1DMem2Mem, CcuTempAllGatherNHR1DMultiJettyMem2Mem);
+    InsV2AllGatherParallelExecutor, TopoMatchUBX, CcuTempAllGatherMesh1DMem2Mem, CcuTempAllGatherNHR1DMem2Mem); //CcuTempAllGatherNHR1DMultiJettyMem2Mem
 #endif /* CANN_VERSION_NUM >= CANN_VERSION(9, 0, 0) */
 
 #endif
