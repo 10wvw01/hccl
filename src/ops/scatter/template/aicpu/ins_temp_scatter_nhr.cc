6a498bcd1633e9f9d1fd1329f73b817380e07518
sakura_pc <yaopengcheng3@huawei.com>
2026-06-25 21:34:37 +0800

    scatter适配多jetty

--- src/ops/scatter/template/aicpu/ins_temp_scatter_nhr.cc
+++ src/ops/scatter/template/aicpu/ins_temp_scatter_nhr.cc
@@ -11,6 +11,9 @@
 #include "ins_temp_scatter_nhr.h"
 
 namespace ops_hccl {
+namespace {
+constexpr u32 SMALL_COUNT_512KB = 512 * 1024;
+}
 InsTempScatterNHR::InsTempScatterNHR(const OpParam& param, const u32 rankId, // 传通信域的rankId，userRank
                                         const std::vector<std::vector<u32>> &subCommRanks)
                                         : InsAlgTemplateBase(param, rankId, subCommRanks)
@@ -26,7 +29,7 @@ u64 InsTempScatterNHR::GetThreadNum() const
 }
 
 void InsTempScatterNHR::SetRoot(u32 root)
-{   
+{
     HCCL_INFO("[InsTempScatterNHR][SetRoot] myRank_ [%u], set root_ [%u] ", myRank_, root);
     root_ = root;
 }
@@ -38,8 +41,12 @@ HcclResult InsTempScatterNHR::CalcRes(HcclComm comm, const OpParam& param, const
 
     std::vector<HcclChannelDesc> level0Channels;
     std::vector<HcclChannelDesc> myChannelDescs;
+    u64 perDataSize = DATATYPE_SIZE_TABLE[param.DataDes.dataType];
+    u64 dataSize = param.DataDes.count * perDataSize;
     if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS && !topoInfo->level0PcieMix) {
-        CHK_RET(CalcChannelRequestNHRWithPriorityTopo(comm, param, topoInfo, subCommRanks_, myChannelDescs, CommTopo::COMM_TOPO_CLOS));
+        bool isIsolation =
+            !(IsAllConnetedWithTopo(topoInfo, 0, CommTopo::COMM_TOPO_1DMESH) || dataSize < SMALL_COUNT_512KB);
+        CHK_RET(CalcChannelRequestNhrMultiJetty(comm, param, topoInfo, subCommRanks_, myChannelDescs, isIsolation));
         for (auto channel : myChannelDescs) {
             if (channel.channelProtocol == COMM_PROTOCOL_UBC_CTP) {
                 level0Channels.push_back(channel);
@@ -50,6 +57,12 @@ HcclResult InsTempScatterNHR::CalcRes(HcclComm comm, const OpParam& param, const
     }
     resourceRequest.channels.push_back(level0Channels);
     channelsPerRank_ = CalcChannelsPerRank(level0Channels);
+    if (channelsPerRank_ > MAX_JETTY_NUM) {
+        HCCL_ERROR(" %s  channelsPerRank_ %u is greater than MAX_JETTY_NUM %u",
+            __func__, channelsPerRank_, MAX_JETTY_NUM);
+    } else {
+        HCCL_DEBUG(" %s channelsPerRank_ is %u ", __func__, channelsPerRank_);
+    }
     CHK_RET(GetRes(resourceRequest));
     return HCCL_SUCCESS;
 }
