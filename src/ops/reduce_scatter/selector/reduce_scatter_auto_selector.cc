2d2a6c47bae65ae9ea3506f7914cb43fdf8088ea
weixin_43960572 <yuantangzhi@huawei.com>
2026-06-25 19:47:56 +0800

    Reduce Scatter大于等于512卡走NHR算法

--- src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
+++ src/ops/reduce_scatter/selector/reduce_scatter_auto_selector.cc
@@ -24,6 +24,7 @@ constexpr u64 RS_CCU_64P_MIN_DATA_SIZE = 128 * 1024 * 1024;
 constexpr u64 RS_CCU_64P_SEQ_DATA_SIZE = 16 * 1024 * 1024;
 constexpr u64 RS_CCU_8P_MIN_DATA_SIZE = 64 * 1024 * 1024;
 constexpr u64 RS_AICPU_SEQUENCE_SIZE_THRESHOLD = 4ULL * 1024 * 1024 * 1024;
+constexpr u32 RS_AICPU_LARGE_RANK_SIZE_THRESHOLD = 64;
 
 SelectorStatus ReduceScatterAutoSelector::SelectCcuMsAlgo(const TopoInfoWithNetLayerDetails* topoInfo, const OpParam &opParam,
                                                     const std::map<HcclCMDType, std::vector<HcclAlgoType>> &configAlgMap,
@@ -281,11 +282,13 @@ SelectorStatus ReduceScatterAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetL
         } else if (topoInfo->Level0Nhr) {
             selectAlgName = "InsReduceScatterNHR"; // InsReduceScatterParallelNHRNHR备用
         } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer.at(0) > 1 && topoInfo->level0Topo == Level0Shape::MESH_1D) {
-            if (dataSize > RS_AICPU_1D_MIN_DATA_SIZE) {
+            if (topoInfo->userRankSize > RS_AICPU_LARGE_RANK_SIZE_THRESHOLD || dataSize <= RS_AICPU_1D_MIN_DATA_SIZE) {
+                selectAlgName = "InsReduceScatterNHR";
+                HCCL_INFO("[ReduceScatterAutoSelector] rankSize[%u] > %u or dataSize[%llu] <= min, select [%s]",
+                    topoInfo->userRankSize, RS_AICPU_LARGE_RANK_SIZE_THRESHOLD, dataSize, selectAlgName.c_str());
+            } else {
                 selectAlgName = (dataSize * topoInfo->userRankSize > RS_AICPU_SEQUENCE_SIZE_THRESHOLD) ?
                     "InsReduceScatterSequenceMesh1DNhr" : "InsReduceScatterParallelMesh1DNHR";
-            } else {
-                selectAlgName = "InsReduceScatterNHR";
             }
         } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer.at(0) == 1 || topoInfo->level0Topo == Level0Shape::CLOS) {
             selectAlgName = "InsReduceScatterNHR"; // InsReduceScatterParallelNHRNHR备用
