9cd2e3343780a804199d3dca845ed2a5e6fb58fb
XuanYang11 <yangxuan77@h-partners.com>
2026-06-25 17:32:14 +0800

    缩减find_cann_package依赖包的个数 整改检视意见

--- src/common/hcomm_dlsym/hccl_kernel_compat.cmake
+++ src/common/hcomm_dlsym/hccl_kernel_compat.cmake
@@ -29,11 +29,11 @@ target_compile_options(hccl_kernel_compat PRIVATE
 )
 
 target_link_libraries(hccl_kernel_compat PRIVATE
-    unified_dlog
     $<BUILD_INTERFACE:runtime_headers>
     $<BUILD_INTERFACE:mmpa_headers>
     $<BUILD_INTERFACE:msprof_headers>
-    $<BUILD_INTERFACE:hcomm_headers>
+    $<BUILD_INTERFACE:hcomm_headers>    
+    unified_dlog
 )
 
 target_link_options(hccl_kernel_compat PRIVATE
