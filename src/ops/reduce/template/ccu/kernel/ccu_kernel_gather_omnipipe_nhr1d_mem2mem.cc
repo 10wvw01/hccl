a498ad002d4bb1bfd66729f5fdafd899cfa0d414
unknown <zhangqian305@h-partners.com>
2026-06-26 17:32:13 +0800

    reduce_nhr

--- src/ops/reduce/template/ccu/kernel/ccu_kernel_gather_omnipipe_nhr1d_mem2mem.cc
+++ src/ops/reduce/template/ccu/kernel/ccu_kernel_gather_omnipipe_nhr1d_mem2mem.cc
@@ -183,8 +183,8 @@ CcuResult CcuGatherOmniPipeNHR1DMem2MemKernel(CcuKernelArg arg)
 
     HCCL_INFO("[CcuGatherOmniPipeNHR1DMem2Mem] GatherOmniPipeNHR1DMem2Mem run");
     CCU_CHK_RET(ParseKernelArg(ctx, kernelArg));
-    // CCU_CHK_RET(InitResource(ctx));
-    // CCU_CHK_RET(LoadArgs(ctx));
+    CCU_CHK_RET(InitResource(ctx));
+    CCU_CHK_RET(LoadArgs(ctx));
     
     CCU_CHK_RET(PreSync(ctx));
     
