6fcdba35e2942483abccc1aa5c12fb14bd2b165f
libohao6 <libohao3@huawei.com>
2026-06-26 15:04:11 +0800

    fix: use GetToken instead of HcommCcuGetMemToken to fix compile error

--- src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_v_mesh_1D.cc
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_v_mesh_1D.cc
@@ -149,8 +149,10 @@ HcclResult CcuTempAlltoAllVMesh1D::FastLaunch(const OpParam& param, const Templa
     uint64_t dataTypeSize =  SIZE_TABLE[dataType];
 
     uint64_t freshToken = 0;
-    CcuResult tokenRet = HcommCcuGetMemToken(PointerToAddr(tempFastLaunchCtx.buffInfo.inputPtr),
-                                             static_cast<uint64_t>(param.inputSize), &freshToken);
+    BuffInfo freshBuffInfo = tempFastLaunchCtx.buffInfo;
+    freshBuffInfo.inputSize = param.inputSize;
+    freshBuffInfo.outputSize = param.outputSize;
+    HcclResult tokenRet = GetToken(freshBuffInfo, freshToken);
     HCCL_INFO("[CcuTempAlltoAllVMesh1D::FastLaunch][CACHE_READ] cachedToken(args[2])[%llu], freshToken[%llu], "
               "cachedInputAddr(args[0])[%llu], currentInputPtr[%llu], currentInputSize[%llu], "
               "cachedOutputAddr(args[1])[%llu], currentOutputPtr[%llu], currentOutputSize[%llu], tokenRet[%d]",
