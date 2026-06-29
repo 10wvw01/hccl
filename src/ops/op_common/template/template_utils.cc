b4e4ca958c9c3894829967538458edfc0ad940be
hanyan233 <hanyan36@huawei.com>
2026-06-29 15:30:27 +0800

    fix_util_tbc

--- src/ops/op_common/template/template_utils.cc
+++ src/ops/op_common/template/template_utils.cc
@@ -82,15 +82,11 @@ HcclResult CalcDataSplitByPortGroupCommon(const u64 totalDataCount,
 }
 
 HcclResult CalcPortNum(const std::vector<ChannelInfo>&channels,  std::vector<u8> &diePortGroupSize){
-    CHK_PRT_RET(channels.size() != DIE_NUM_1 && channels.size() != DIE_NUM_2, HCCL_ERROR("[CalcPortNum] channels.size is %u, which is wrong.",
-                channels.size()), HcclResult::HCCL_E_INTERNAL);
-    if(channels.size() == DIE_NUM_1){
-        CHK_PRT_RET(channels.size() != DIE_NUM_1, HCCL_ERROR("[CalcPortNum] channels.size %u, which is not equal to 1.",
-            channels.size), HcclResult::HCCL_E_INTERNAL);
+    const chSize = channels.size();
+    CHK_PRT_RET(chSize != DIE_NUM_1 && chSize != DIE_NUM_2, HCCL_ERROR("[CalcPortNum] channels.size is %u, which is wrong.",chSize), HcclResult::HCCL_E_INTERNAL);
+    if(chSize == DIE_NUM_1){
         diePortGroupSize = {1, 0};
-    } else if (channels.size() == DIE_NUM_2) {
-         CHK_PRT_RET(channels.size() != DIE_NUM_2, HCCL_ERROR("[CalcPortNum] channels.size %u, which is not equal to 2.",
-            channels.size()), HcclResult::HCCL_E_INTERNAL);
+    } else if (chSize == DIE_NUM_2) {
         for (u32 i = 0; i < DIE_NUM_2; i++) {
             diePortGroupSize[i] = channels[i].portGroupSize;
             HCCL_DEBUG("[CalcPortNum] diePortGroupSize[%u]=[%u]", i, diePortGroupSize[i]);
