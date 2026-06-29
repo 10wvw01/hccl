20c4d29c6192ded24ca50dd0e4e44150cad7d6f0
XuanYang11 <yangxuan77@h-partners.com>
2026-06-29 15:08:34 +0800

    更新cmake仓tag

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -15,12 +15,12 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-015")
+        set(CANN_CMAKE_TAG "master-033")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
                 URL "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz"
-                URL_HASH SHA256=b8827cb7d30cbf1a6e40d79bca1910dc911dc05058563984b5f1ee104a1933e3
+                URL_HASH SHA256=82c08ae7df8bf291321f33b0c6c0b3e6ff328ba945984960df3c65d07bff9d8c
             )
         else()
             FetchContent_Declare(
