8c08ffd6509dc99baa9c01e2f4b51c875b900785
shirokawa_ <jinliuyi@huawei.com>
2026-07-01 15:56:23 +0800

    add

--- src/ops/op_common/template/aiv/hccl_aiv_utils.cc
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.cc
@@ -54,6 +54,16 @@ DECL_AIV_EMBED(hccl_aiv_reduce_scatter_op_910_95);
 DECL_AIV_EMBED(hccl_aiv_scatter_op_910_95);
 DECL_AIV_EMBED(hccl_aiv_send_op_910_95);
 DECL_AIV_EMBED(hccl_aiv_recv_op_910_95);
+DECL_AIV_EMBED(hccl_aiv_all_gather_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_all_reduce_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_all_to_all_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_all_to_all_v_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_broadcast_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_reduce_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_reduce_scatter_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_scatter_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_send_op_910_96);
+DECL_AIV_EMBED(hccl_aiv_recv_op_910_96);
 #undef DECL_AIV_EMBED
 }
 
@@ -77,6 +87,16 @@ static const AivEmbedSymbol kAivEmbedTable[] = {
     EMBED_ENTRY(hccl_aiv_scatter_op_910_95),
     EMBED_ENTRY(hccl_aiv_send_op_910_95),
     EMBED_ENTRY(hccl_aiv_recv_op_910_95),
+    EMBED_ENTRY(hccl_aiv_all_gather_op_910_96),
+    EMBED_ENTRY(hccl_aiv_all_reduce_op_910_96),
+    EMBED_ENTRY(hccl_aiv_all_to_all_op_910_96),
+    EMBED_ENTRY(hccl_aiv_all_to_all_v_op_910_96),
+    EMBED_ENTRY(hccl_aiv_broadcast_op_910_96),
+    EMBED_ENTRY(hccl_aiv_reduce_op_910_96),
+    EMBED_ENTRY(hccl_aiv_reduce_scatter_op_910_96),
+    EMBED_ENTRY(hccl_aiv_scatter_op_910_96),
+    EMBED_ENTRY(hccl_aiv_send_op_910_96),
+    EMBED_ENTRY(hccl_aiv_recv_op_910_96),
 };
 #undef EMBED_ENTRY
 
@@ -613,7 +633,10 @@ HcclResult RegisterKernel()
         return HCCL_SUCCESS;
     }
 
-    for (const auto& item : g_aivKernelInfoMap) {
+    DevType deviceType = DevType::DEV_TYPE_COUNT;
+    CHK_RET(hrtGetDeviceType(deviceType));
+
+    for (const auto& item : GetAivKernelInfoMap(deviceType)) {
         const HcclCMDType cmdType = item.first;
         const std::string& aivBinaryName = item.second.first;
         const std::vector<AivKernelInfo>& aivKernelInfoList = item.second.second;
