75e05ab7f724d61686d7394c5614b08dddf349e6
yjzz1007 <zhaojiayu7@huawei.com>
2026-07-02 19:37:27 +0800

    alltoallv ccu opt

--- src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_v_mesh_1D.cc
+++ src/ops/all_to_all_v/template/ccu/ccu_temp_all_to_all_v_mesh_1D.cc
@@ -52,9 +52,10 @@ HcclResult CcuTempAlltoAllVMesh1D::CalcChannelRes(HcclComm comm, const OpParam& 
     } else {
         if(topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS && !topoInfo->level0PcieMix) {
             CHK_RET(CalcChannelRequestMeshClosMultiJetty(comm, param, topoInfo, subCommRanks_, channelDescs));
+            HCCL_DEBUG("[CcuTempAlltoAllVMesh1D::CalcRes] Get Channels size [%u]", channelDescs.size());
         } else {
             CHK_RET(CalcChannelRequestMesh1D(comm, param, topoInfo, subCommRanks_, channelDescs));
-    }
+        }
     }
     return HCCL_SUCCESS;
 }
