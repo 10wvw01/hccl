fc47c9578427632138802206b04922c3cf0e73ca
xiaocui235 <cuiruxing@huawei.com>
2026-07-02 22:44:27 +0800

    selector

--- src/ops/broadcast/selector/broadcast_auto_selector.cc
+++ src/ops/broadcast/selector/broadcast_auto_selector.cc
@@ -125,20 +125,20 @@ SelectorStatus BroadcastAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayer
     (void)configAlgMap;
     HCCL_DEBUG("[BroadcastAutoSelector][%s] start, topoInfo levelNum[%u]", __func__, topoInfo->topoLevelNums);
     if (topoInfo->topoLevelNums > 1) {
-        if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3) {
+        if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3 && topoInfo->level2Uboe) {
             if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[1] == 1) {
                 selectAlgName = "InsBroadcastNHR";
-            } else if (topoInfo->level2Uboe) {
-                selectAlgName = "InsBroadcastParallelNHRNHRUboe";
-            } else if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
-                selectAlgName = "InsBroadcastSequenceMesh1DNHRNHR";
             } else {
                 selectAlgName = "InsBroadcastParallelNHRNHRUboe";
             }
         } else if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[0] == 1) {
             selectAlgName = "InsBroadcastNHR";
         } else if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
-            selectAlgName = "InsBroadcastParallelMesh1DNHR";
+            if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3) {
+                selectAlgName = "InsV2AllReduceSequenceMesh1DNHRNHR";
+            } else {
+                selectAlgName = "InsBroadcastParallelMesh1DNHR";
+            }
         } else if (topoInfo->level0Topo == Level0Shape::CLOS) {
             selectAlgName = "InsBroadcastNHR";
         } else {
