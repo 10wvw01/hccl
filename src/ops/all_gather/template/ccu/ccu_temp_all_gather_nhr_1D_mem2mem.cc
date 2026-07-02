30c70862aa636c8738b25dd44fc959353b7a3055
hanyan233 <hanyan36@huawei.com>
2026-07-02 19:34:56 +0800

    fix_compile

--- src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.cc
+++ src/ops/all_gather/template/ccu/ccu_temp_all_gather_nhr_1D_mem2mem.cc
@@ -132,7 +132,6 @@ HcclResult CcuTempAllGatherNHR1DMem2Mem::CalcRes(HcclComm comm, const OpParam& p
         CHK_RET(GetChannelBwCoeff(comm, myRank_, channelsPerDie[1][0], p1));
         if (p0 + p1 > 0) {
             ratio = static_cast<double>(p0) / (p0 + p1);
-            HCCL_INFO("HYRATIO=%llu",ratio);
         }
     }
     resourceRequest.dieSplitRatio = ratio;
@@ -192,7 +191,6 @@ HcclResult CcuTempAllGatherNHR1DMem2Mem::SplitDataFor2Dies(const OpParam& param,
     }
 
     die0Size = static_cast<uint64_t>(dataCount * dieSplitRatio_) * typeSize;
-    HCCL_INFO("HYdieSplitRatio_=%llu",dieSplitRatio_);
     die1Size = templateDataParams.sliceSize - die0Size;
     HCCL_DEBUG("[CcuTempAllGatherNHR1DMem2Mem::SplitDataFor2Dies] die0Size = %llu, die1Size = %llu", die0Size , die1Size);
     return HcclResult::HCCL_SUCCESS;
