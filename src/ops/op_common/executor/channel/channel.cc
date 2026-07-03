c91f0bb094707f9ca2b4fe6dbee7652a0306dbfe
hanyan233 <hanyan36@huawei.com>
2026-07-03 14:09:24 +0800

    fix_channelperdie

--- src/ops/op_common/executor/channel/channel.cc
+++ src/ops/op_common/executor/channel/channel.cc
@@ -98,7 +98,7 @@ HcclResult ProcessFlattenLink(HcclComm comm, u32 myRank, const std::vector<std::
 {
 #if !defined(AICPU_COMPILE) && (CANN_VERSION_NUM >= CANN_VERSION(9, 0, 0))
     std::map<u32, std::vector<HcclChannelDesc>> rankIdToChannelDesc;
-    CHK_RET(CcuAlgTemplateBase::RestoreChannelMap(channels, rankIdToChannelDesc));
+    CHK_RET(CcuAlgTemplateBase::RestoreChannelMap(comm, myRank, channels, rankIdToChannelDesc));
     uint32_t enableDieNum = 0;
     uint32_t enableDieId = 0;
     CHK_RET(CcuAlgTemplateBase::GetDieInfoFromChannelDescs(comm, rankIdToChannelDesc, myRank, enableDieNum, enableDieId));
