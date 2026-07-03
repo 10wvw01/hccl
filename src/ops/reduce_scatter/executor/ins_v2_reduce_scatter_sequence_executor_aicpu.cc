619fb665d1d67f197fffd6360393598c7ac5cac6
gcw_dFpssWuk <wanghaixu5@h-partners.com>
2026-07-03 14:17:59 +0800

    fix master sc

--- src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_aicpu.cc
+++ src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_sequence_executor_aicpu.cc
@@ -170,7 +170,6 @@ void InsV2ReduceScatterSequenceExecutorAicpu<AlgTopoMatch, InsAlgTemplate0, InsA
     } else {
         tempAlgParamsIntra.buffInfo.hcclBuffBaseOff = 0;
     }
-    
 
     tempAlgParamsIntra.sliceSize = currDataCount * dataTypeSize_;
     tempAlgParamsIntra.tailSize = tempAlgParamsIntra.sliceSize;
