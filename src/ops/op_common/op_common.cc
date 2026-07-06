2f41ecc4fcb57dd25227a5162ec731d25ffaa942
renyumin <renyumin1@huawei.com>
2026-07-06 09:27:57 +0800

    [fix]InconsistentCheckJudgmentLogicChange

--- src/ops/op_common/op_common.cc
+++ src/ops/op_common/op_common.cc
@@ -56,6 +56,7 @@
 #include "comm_engine_utils.h"
 
 namespace ops_hccl {
+thread_local bool needInconsistentCheck = false;
 // 用于维护增量建链算子的host ctx信息
 constexpr u32 HOST_WAIT_AICPU_NOTIFYIDX = 0;// host主流wait aicpu流的notify idx
 constexpr u32 HOST_NOTIFY_TIMEOUT_OFFSET = 27;  // host等待Device通知的超时时间偏移量
@@ -986,6 +987,9 @@ HcclResult HcclGetAlgRes(HcclComm comm, OpParam& param, std::unique_ptr<InsCollA
         return HCCL_SUCCESS;
     }
 
+    // context创建前做是否需要参数一致性的判断，context未创建则判断为首次下发该算子
+    needInconsistentCheck = NeedInconsistentCheck(comm, param);
+
     // 计算AlgHierarchyInfo
     AlgHierarchyInfoForAllLevel algHierarchyInfo;  // 分级通信域信息{localRankId, localRankSize}
     CHK_RET(executor->CalcAlgHierarchyInfo(comm, topoInfo, algHierarchyInfo));
@@ -1013,7 +1017,7 @@ HcclResult HcclGetAlgRes(HcclComm comm, OpParam& param, std::unique_ptr<InsCollA
     }
 
     // 参数一致性校验
-    if (NeedInconsistentCheck(param)) {
+    if (needInconsistentCheck) {
         OpExchangeInfo exchangeInfo{};
         CHK_RET(FillOpExchangeInfo(comm, param, exchangeInfo));
         CHK_RET(CompareOpExchangeInfos(comm, param, resRequest, exchangeInfo));
@@ -1032,7 +1036,7 @@ HcclResult FillOpExchangeInfo(HcclComm comm, const OpParam &param, OpExchangeInf
     exchangeInfo.opExecuteConfig = param.opExecuteConfig;
     exchangeInfo.reduceType = param.reduceType;
     CHK_RET(FillOpExchangeInfoWithDataDes(param, exchangeInfo));
-    if (param.opMode == OpMode::OFFLOAD) {
+    if (param.opMode == OpMode::OFFLOAD && param.engine == CommEngine::COMM_ENGINE_AIV) {
         AivParamStorage *aivParam = nullptr;
         HcclResult ret = GetAivParamStorageByComm(comm, &aivParam);
         if (ret == HCCL_SUCCESS && aivParam != nullptr) {
@@ -1084,7 +1088,7 @@ HcclResult FillOpExchangeInfoWithDataDes(const OpParam &param, OpExchangeInfo &e
 HcclResult AddExchangeInfo(HcclComm comm, const OpParam &param)
 {
     CHK_PTR_NULL(comm);
-    if (NeedInconsistentCheck(param)) {
+    if (needInconsistentCheck) {
         OpExchangeInfo exchangeInfo{};
         CHK_RET(FillOpExchangeInfo(comm, param, exchangeInfo));
         CHK_RET(HcclCommAddExchangeInfo(comm, &exchangeInfo, sizeof(exchangeInfo)));
