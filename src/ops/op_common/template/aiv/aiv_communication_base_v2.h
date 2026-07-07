934564172878f4acd3c64133242741f1bd609ae5
yan674228356 <yan674228356@163.com>
2026-07-07 12:30:13 +0800

    fix: 修正变量名 seperateOffset 拼写错误为 separateOffset
    将 aiv_communication_base_v2.h 中的变量名 seperateOffset 修正为
    separateOffset（separate 的正确拼写），涉及3处引用。
    
    Signed-off-by: yan674228356 <yan674228356@163.com>

--- src/ops/op_common/template/aiv/aiv_communication_base_v2.h
+++ src/ops/op_common/template/aiv/aiv_communication_base_v2.h
@@ -201,7 +201,7 @@ public:
         multiOffset = MAX_NUM_BLOCKS * DOUBLE * FLAG_SIZE+ localOffset;
         pingpongOffset = multiOffset + DOUBLE * DOUBLE * NUM_BLOCKS_FOUR_PER_RANK_A3 * ATOMIC_FLAG_SIZE * DOUBLE;
         countOffset = DOUBLE * pingpongOffset;
-        seperateOffset = countOffset + NUM_BLOCKS_FOUR_PER_RANK_A3 * rankSize_ * FLAG_SIZE;
+        separateOffset = countOffset + NUM_BLOCKS_FOUR_PER_RANK_A3 * rankSize_ * FLAG_SIZE;
 
         pipe.InitBuffer(localFlagBuf, LOCAL_FLAG_BUF_LEN);
         localSetTensor = localFlagBuf.GetWithOffset<int32_t>(UB_FLAG_PAD_COUNT, FLAG_ONE_OFFSET);
@@ -246,7 +246,7 @@ public:
         multiOffset = MAX_NUM_BLOCKS * DOUBLE * FLAG_SIZE+ localOffset;
         pingpongOffset = multiOffset + DOUBLE * DOUBLE * NUM_BLOCKS_FOUR_PER_RANK_A3 * ATOMIC_FLAG_SIZE * DOUBLE;
         countOffset = DOUBLE * pingpongOffset;
-        seperateOffset = countOffset + NUM_BLOCKS_FOUR_PER_RANK_A3 * rankSize_ * FLAG_SIZE;
+        separateOffset = countOffset + NUM_BLOCKS_FOUR_PER_RANK_A3 * rankSize_ * FLAG_SIZE;
 
         InitBuffArray(args->buffersIn);
 
@@ -395,7 +395,7 @@ public:
     uint32_t multiOffset;
     uint32_t pingpongOffset;
     uint32_t countOffset;
-    uint32_t seperateOffset;
+    uint32_t separateOffset;
 };
 
 
