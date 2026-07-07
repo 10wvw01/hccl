e1467fe9067b01c1140640e8eab6027580baa195
hanyan233 <hanyan36@huawei.com>
2026-07-07 19:16:27 +0800

    ata2plis6

--- src/ops/op_common/inc/alg_param.h
+++ src/ops/op_common/inc/alg_param.h
@@ -345,6 +345,7 @@ struct CcuFastLaunchCtx {
 
 // A5用了cntNotify
 struct AlgResourceRequest {
+    double dieSplitRatio = 0.0;
     u32 notifyNumOnMainThread = 0;
     u32 slaveThreadNum = 0;
     std::vector<u32> notifyNumPerThread;
@@ -418,6 +419,7 @@ struct AlgResourceCtxSerializable {
     std::vector<ThreadHandle> threads;
     ThreadHandle unfoldThread = 0; // 展开流thread
     std::vector<std::vector<ChannelInfo>> channels;
+    double dieSplitRatio = 0.0;
     bool isHcommBatchTransferOnThreadSupported = false;
     bool isHcclThreadAcquireWithConfigSupported = false;
     void* commInfoPtr = nullptr;
@@ -454,6 +456,7 @@ struct AlgResourceCtxSerializable {
 
         binaryStream << ccuKernelNum;
         binaryStream << ccuKernels;
+        binaryStream << dieSplitRatio;
         std::vector<char> seq = topoInfo.Serialize();
         topoInfoSeqSize = seq.size();
         binaryStream << topoInfoSeqSize;
@@ -488,6 +491,7 @@ struct AlgResourceCtxSerializable {
 
         binaryStream >> ccuKernelNum;
         binaryStream >> ccuKernels;
+        binaryStream >> dieSplitRatio;
         binaryStream >> topoInfoSeqSize;
         size_t startPos = data.size() - topoInfoSeqSize;
         std::vector<char> tailData(data.begin() + startPos, data.end());
