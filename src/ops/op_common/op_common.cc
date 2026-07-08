e49b484f15970b2ffa87fb603ebf232d1db779d8
liujt_ <liujintao28@huawei.com>
2026-07-08 16:31:31 +0800

    fix profiling bug

--- src/ops/op_common/op_common.cc
+++ src/ops/op_common/op_common.cc
@@ -702,7 +702,13 @@ HcclResult HcclExecOp(HcclComm comm, OpParam &param,
         CHK_RET(executor->Orchestrate(param, *resCtxHost));
     }
     // op上报
-    CHK_RET(HcclProfilingReportOp(comm, beginTime));
+    bool isGroupEnabled = false;
+    if (HcommIsSupportHcclGroupStatusGet()) {
+        CHK_RET(HcclGroupStatusGet(&isGroupEnabled));
+    }
+    if (!isGroupEnabled) {
+        CHK_RET(HcclProfilingReportOp(comm, beginTime));
+    }
     HCCL_INFO("Execute HcclExecOp success.");
     return HCCL_SUCCESS;
 }
@@ -840,6 +846,7 @@ HcclResult HcclAicpuKernelEntranceLaunch(HcclComm comm, OpParam &param, ThreadHa
         HCCL_ERROR("[HcclAicpuKernelEntranceLaunch] HcclReportAicpuKernel failed, beginTime %lu, kernelNameCStr %s, ret %d ", beginTime, kernelNameCStr, ret);
         return ret;
     }
+
     // Host stream等待Device的通知
     AicpuTimeout timeout = DeriveAicpuTimeout(param.opConfig.execTimeout);
     u32 hostNotifyWaitTime = IsHcommDefaultTimeoutSupported() ? timeout.hostNotifyTimeout :
