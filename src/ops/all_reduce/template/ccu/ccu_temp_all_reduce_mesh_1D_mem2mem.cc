27cef0d0660c2d93fe5f65615189a7d79b50463d
Xie21 <2578926836@qq.com>
2026-07-08 19:18:48 +0800

    update: 更新文件 ccu_temp_all_reduce_mesh_1D_mem2mem.cc
    Signed-off-by: Xie21 <2578926836@qq.com>

--- src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D_mem2mem.cc
+++ src/ops/all_reduce/template/ccu/ccu_temp_all_reduce_mesh_1D_mem2mem.cc
@@ -44,10 +44,10 @@ HcclResult CcuTempAllReduceMeshMem2Mem1D::CalcSlice(const u64 dataSize, RankSlic
     std::vector<SliceInfo> tmp(subCommRanks_.size());
     sliceInfoVec.resize(templateRankSize_, tmp);
 
-    u64 unitAllignSize = DataTypeSizeGet(dataType_);
-    u64 chunkSize      = RoundUp(dataSize, (templateRankSize_ * unitAllignSize)) * unitAllignSize;
-    HCCL_INFO("[CcuTempAllReduceMeshMem2Mem1D] chunkSize[%llu], dataSize[%llu], templateRankSize_[%u], unitAllignSize[%llu]",
-              chunkSize, dataSize, templateRankSize_, unitAllignSize);
+    u64 unitAlignSize = DataTypeSizeGet(dataType_);
+    u64 chunkSize      = RoundUp(dataSize, (templateRankSize_ * unitAlignSize)) * unitAlignSize;
+    HCCL_INFO("[CcuTempAllReduceMeshMem2Mem1D] chunkSize[%llu], dataSize[%llu], templateRankSize_[%u], unitAlignSize[%llu]",
+              chunkSize, dataSize, templateRankSize_, unitAlignSize);
     u64 accumOff = 0;
     for (u32 rankIdx = 0; rankIdx < templateRankSize_; rankIdx++) {
         u64       currChunkSize  = ((dataSize - accumOff) > chunkSize) ? chunkSize : (dataSize - accumOff);
