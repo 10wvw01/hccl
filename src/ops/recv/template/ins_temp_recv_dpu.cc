364b03a333a00d4c1139ff17b1fa2074a3c553cb
xiehuijiao <x00958740@china.huawei.com>
2026-07-09 21:13:16 +0800

    Fix SendRecv DPU channel request scope

--- src/ops/recv/template/ins_temp_recv_dpu.cc
+++ src/ops/recv/template/ins_temp_recv_dpu.cc
@@ -34,7 +34,8 @@ namespace ops_hccl
         resourceRequest.notifyNumPerThread = {};
         resourceRequest.notifyNumOnMainThread = 0;
         std::vector<HcclChannelDesc> level0Channels;
-        CHK_RET(CalcChannelRequestMesh1D(comm, param, topoInfo, subCommRanks_, level0Channels));
+        static_cast<void>(topoInfo);
+        CHK_RET(CreateChannelRequestByRankId(comm, param, myRank_, param.sendRecvRemoteRank, level0Channels));
         resourceRequest.channels.push_back(level0Channels);
         HCCL_INFO("[InsTempRecvDpu][CalcRes] Successfully calres!");
         return HCCL_SUCCESS;
