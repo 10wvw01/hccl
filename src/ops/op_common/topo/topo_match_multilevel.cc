f5bc3bb9ba468150250f4c0fc4c7bf9da246b2da
chengym <chengyueming@huawei.com>
2026-07-09 14:56:07 +0800

    not run ubx while topo info has pciemix

--- src/ops/op_common/topo/topo_match_multilevel.cc
+++ src/ops/op_common/topo/topo_match_multilevel.cc
@@ -89,9 +89,18 @@ HcclResult TopoMatchMultilevel::TopoForLayer0(
                 ranks_y.assign(ranks, ranks + rankNum);
             }
         }
-        algHierarchyInfo.infos[0].push_back(ranks_x);
-        algHierarchyInfo.infos[0].push_back(ranks_y);
-        layer0Size = ranks_x.size() * ranks_y.size();
+        if (ranks_x.size() != 0) {
+            algHierarchyInfo.infos[0].push_back(ranks_x);
+            layer0Size = ranks_x.size();
+        }
+        if (ranks_y.size() != 0) {
+            algHierarchyInfo.infos[0].push_back(ranks_y);
+            if (layer0Size == 0) {
+                layer0Size = ranks_y.size();
+            } else {
+                layer0Size *= ranks_y.size();
+            }
+        }
     }
 #endif
     return HcclResult::HCCL_SUCCESS;
@@ -124,6 +133,10 @@ HcclResult TopoMatchMultilevel::TopoForLayer1(
         if (myRank == rankId) {
             rankVecLayer1WithSameIdx.push_back(rankId);
             continue;
+        }
+        if (layer0Size == 0) {
+            HCCL_ERROR("[TopoMatchMultilevel::MeshNHRTopoForLayer1] layer0Size is 0, Invalid topo.");
+            return HcclResult::HCCL_E_PARA;
         }
         if (rankId % layer0Size != myRank % layer0Size) {
             continue;
@@ -200,6 +213,10 @@ HcclResult TopoMatchMultilevel::TopoForLayer2(
         if (myRank == rankId) {
             rankVecLayer2WithSameIdx.push_back(rankId);
             continue;
+        }
+        if (layer0Size == 0 || layer1Size == 0) {
+            HCCL_ERROR("[TopoMatchMultilevel::TopoForLayer2] layer0Size or layer1Size is 0, Invalid topo.");
+            return HcclResult::HCCL_E_PARA;
         }
         if (rankId % (layer0Size * layer1Size) != myRank % (layer0Size * layer1Size)) {
             continue;
--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -357,6 +357,16 @@ SelectorStatus ReduceAutoSelector::SelectDPUAlgo(const TopoInfoWithNetLayerDetai
             selectAlgName = "InsReduceSequenceMeshNhrDPU";
             HCCL_INFO("selectAlgName is InsReduceSequenceMeshNhrDPU");
             return SelectorStatus::MATCH;
+        } else if (topoInfo->level0Topo == Level0Shape::MESH_1D_CLOS) {
+            if (!topoInfo->level0PcieMix) {
+                selectAlgName = "InsV2ReduceOmniPipe";
+                HCCL_INFO("selectAlgName is InsV2ReduceOmniPipe");
+                return SelectorStatus::MATCH;
+            } else {
+                selectAlgName = "InsReduceSequenceMeshNhrDPU";
+                HCCL_INFO("selectAlgName is InsReduceSequenceMeshNhrDPU");
+                return SelectorStatus::MATCH;
+            }
         }
     }
     return SelectorStatus::NOT_MATCH;
