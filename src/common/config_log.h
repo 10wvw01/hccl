0a57bc12f37fcf595800aa2e6fb4c8d2834bcc40
linzhenkang <linzhenkang@huawei.com>
2026-07-10 17:23:50 +0800

    修改

--- src/common/config_log.h
+++ src/common/config_log.h
@@ -17,7 +17,7 @@ namespace ops_hccl {
 constexpr u64 HCCL_ALG = 0x1ULL << 0;
 constexpr u64 HCCL_TASK = 0x1ULL << 1;
 constexpr u64 HCCL_RES = 0x1ULL << 2;
-constexpr u64 HCCL_AIV_OPS_EXC = 0x1ULL << 3;
+constexpr u64 HCCL_DATA_OP = 0x1ULL << 3;
 
 u64 GetDebugConfig();
 
