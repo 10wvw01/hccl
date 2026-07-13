a74e3df7736083c00df01e089337f1435cbdf86a
acoolntly <chenjianhui22@h-partners.com>
2026-07-13 20:12:50 +0800

    fix All_reduce && Reduce_scatter 3level

--- src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_omnipipe_executor.cc
+++ src/ops/reduce_scatter/executor/ins_v2_reduce_scatter_omnipipe_executor.cc
@@ -256,9 +256,14 @@ InsV2ReduceScatterOmniPipeExecutor<AlgTopoMatch, InsAlgTemplate0, InsAlgTemplate
 
     // 先初始化remoteRankToChannelInfo_，然后为nhr赋值多channel，最后再计算资源，这样计算线程资源的时候就能获取到多channel需要的线程数
     CHK_RET(RestoreChannelMap(resCtx, remoteRankToChannelInfo_));
-    // todo 这边写死了
+    if (rankSizeLevel0_ > 1) {
+        CHK_RET(tempMap[OMNIPIPE_LEVEL0]->SetchannelsPerRank(remoteRankToChannelInfo_[0]));
+    }
     if (rankSizeLevel1_ > 1) {
-        tempMap[OMNIPIPE_LEVEL1]->SetchannelsPerRank(remoteRankToChannelInfo_[1]);
+        CHK_RET(tempMap[OMNIPIPE_LEVEL1]->SetchannelsPerRank(remoteRankToChannelInfo_[1]));
+    }
+    if (rankSizeLevel2_ > 1) {
+        CHK_RET(tempMap[OMNIPIPE_LEVEL2]->SetchannelsPerRank(remoteRankToChannelInfo_[2]));
     }
 
     for (auto& temp : tempMap) {
