81312eb059c0c24d5e24fb391b4a02ab2ba7d26a
shirokawa_ <jinliuyi@huawei.com>
2026-07-15 12:02:50 +0800

    ADD

--- src/ops/all_reduce/all_reduce_op.cc
+++ src/ops/all_reduce/all_reduce_op.cc
@@ -25,6 +25,7 @@ HcclResult HcclAllReduce(void *sendBuf, void *recvBuf, uint64_t count, HcclDataT
 {
     HCCL_INFO("Start to run execute HcclAllReduce");
     if (GetHcommVersion() < CANN_VERSION(9, 0, 0)) { // compat handle
+        HCCL_INFO("werwer0001");
         return HcclAllReduceInner(sendBuf, recvBuf, count, dataType, op, comm, stream);
     }
 
@@ -35,6 +36,7 @@ HcclResult HcclAllReduce(void *sendBuf, void *recvBuf, uint64_t count, HcclDataT
     #else
     if (deviceType != DevType::DEV_TYPE_910_95) {
     #endif
+        HCCL_INFO("werwer0002");
         return HcclAllReduceInner(sendBuf, recvBuf, count, dataType, op, comm, stream);
     }
     CHK_PRT_RET(count == 0, HCCL_WARNING("input count is 0, return all reduce success"), HCCL_SUCCESS);
@@ -189,6 +191,7 @@ HcclResult AllReduceOutPlaceCommon(void *sendBuf, void *recvBuf, uint64_t count,
     // 9.0.0 ccu模式走老流程
     if (opMode == OpMode::OPBASE && GetHcommVersion() == CANN_VERSION(9, 0, 0) &&
         param.engine == CommEngine::COMM_ENGINE_CCU) {
+        HCCL_INFO("werwer0003");
         return HcclAllReduceInner(sendBuf, recvBuf, count, dataType, op, comm, stream);
     }
 
@@ -209,6 +212,7 @@ HcclResult AllReduceOutPlaceCommon(void *sendBuf, void *recvBuf, uint64_t count,
     std::unique_ptr<TopoInfoWithNetLayerDetails> topoInfo = std::make_unique<TopoInfoWithNetLayerDetails>();
     CHK_RET(Selector(comm, param, topoInfo, algName));
     if (ShouldUseInnerOp(param.opExecuteConfig) && param.opMode == OpMode::OPBASE) {
+        HCCL_INFO("werwer0004");
         return HcclAllReduceInner(sendBuf, recvBuf, count, dataType, op, comm, stream);
     }
     // 单卡校验
