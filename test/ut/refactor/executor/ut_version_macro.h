33e6ddb619162978bcd53a0b594010527051439b
luomin2005 <icecool.luomin@huawei.com>
2026-07-15 10:16:25 +0800

    ut fix

--- test/ut/refactor/executor/ut_version_macro.h
+++
@@ -1,8 +0,0 @@
-#ifndef UT_VERSION_MACRO_H
-#define UT_VERSION_MACRO_H
-
-#ifndef CANN_VERSION
-#define CANN_VERSION(major, minor, patch) ((major) * 10000000 + (minor) * 100000 + (patch) * 1000)
-#endif
-
-#endif
