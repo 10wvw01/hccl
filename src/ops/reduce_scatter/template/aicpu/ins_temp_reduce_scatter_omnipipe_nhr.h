799554fdeb9dd8955c28145860ea0ca36914f9e4
yxs9 <yangxuesong8@huawei.com>
2026-07-15 15:33:21 +0800

    new version

--- src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_omnipipe_nhr.h
+++ src/ops/reduce_scatter/template/aicpu/ins_temp_reduce_scatter_omnipipe_nhr.h
@@ -50,6 +50,9 @@ private:
     std::map<u32, std::vector<ChannelInfo>> channels_;
     std::vector<std::vector<std::vector<u64>>> dataSplitVec_;
     std::vector<std::vector<std::vector<u64>>> dataOffsetVec_;
+    // 对称内存：user input 对称窗口及偏移（KernelRun 从 param 缓存）
+    u64 inputOffset_{0};
+    void *inputSymWindow_{nullptr};
 };
 
 } // namespace Hccl
