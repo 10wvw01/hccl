74dd0560c55edc526f82d4db9040567ccdb5e531
wangyujiang98 <wangyujiang3@h-partners.com>
2026-07-16 16:20:51 +0800

    HCCL order Launch

--- src/common/hcomm_dlsym/hccl_res_dl.cc
+++ src/common/hcomm_dlsym/hccl_res_dl.cc
@@ -31,6 +31,9 @@ DEFINE_WEAK_FUNC(HcclResult, HcclEngineCtxDestroy, HcclComm comm, const char* ct
 DEFINE_WEAK_FUNC(HcclResult, HcclThreadAcquireWithConfig, HcclComm comm, CommEngine engine, uint32_t threadNum,
     ThreadType type, const ThreadConfig *config, ThreadHandle *threads);
 
+DEFINE_WEAK_FUNC(HcclResult, HcclDedicatedThreadAcquire, HcclComm comm, HcclDedicatedThreadType useType,
+    uint32_t notifyNumPerThread, ThreadHandle *thread);
+
 // 初始化
 void HcclResDlInit(void* libHcommHandle) {
     INIT_SUPPORT_FLAG(libHcommHandle, HcclGetRemoteIpcHcclBuf);
@@ -42,4 +45,5 @@ void HcclResDlInit(void* libHcommHandle) {
     INIT_SUPPORT_FLAG(libHcommHandle, HcclCommMemReg);
     INIT_SUPPORT_FLAG(libHcommHandle, HcclEngineCtxDestroy);
     INIT_SUPPORT_FLAG(libHcommHandle, HcclThreadAcquireWithConfig);
+    INIT_SUPPORT_FLAG(libHcommHandle, HcclDedicatedThreadAcquire);
 }
