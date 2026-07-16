5c3aeb5cb8a72435fbfd7e3b6336c85a55d083e7
hanyan233 <hanyan36@huawei.com>
2026-07-16 14:59:03 +0800

    fix_compile_16M

--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -12,7 +12,8 @@
 #include "hccl_aiv_utils.h"
 
 namespace ops_hccl {
-constexpr u64 REDUCE_AICPU_1D_MAX_DATA_SIZE = 16 * 1024 * 1024;
+constexpr u64 REDUCE_AICPU_1D_MAX_DATA_SIZE = 8 * 1024 * 1024;
+constexpr u64 REDUCE_CCU_TWOSHOT_1D_MAX_DATA_SIZE = 16 * 1024 * 1024;
 constexpr u64 REDUCE_NHR_CCU_MAX_DATA_SIZE = 256 * 1024;
 
 SelectorStatus ReduceAutoSelector::SelectCcuMsAlgo(const TopoInfoWithNetLayerDetails *topoInfo, const OpParam &opParam,
@@ -172,7 +173,7 @@ SelectorStatus ReduceAutoSelector::SelectMeshAlgoCcuSchedule(
         if (topoInfo->is2DieFullMesh) {
             HCCL_WARNING("[ReduceAutoSelector] 2DieFullMesh is not supported yet for ccu schedule mode.");
             return SelectorStatus::NOT_MATCH;
-        } else if (dataSize > REDUCE_AICPU_1D_MAX_DATA_SIZE) {
+        } else if (dataSize > REDUCE_CCU_TWOSHOT_1D_MAX_DATA_SIZE) {
             HCCL_INFO("[ReduceAutoSelector] Mesh1D dataSize[%llu] > 16MB, fallback to aicpu.", dataSize);
             return SelectorStatus::NOT_MATCH;
         } else {
