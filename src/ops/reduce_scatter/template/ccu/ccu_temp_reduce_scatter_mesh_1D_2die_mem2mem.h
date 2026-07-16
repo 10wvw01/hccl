75ab11ede42a714ed85f0025992f79e5e5c57003
zhongyu1 <zhongyu37@huawei.com>
2026-07-16 20:16:20 +0800

    rs mesh2die

--- src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h
+++ src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h
@@ -42,8 +42,14 @@ public:
     u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
 
 private:
-    uint32_t localReduceOffset_ = 0;
+    HcclResult PartitionChannels(HcclComm comm, const std::vector<HcclChannelDesc> &channelDescs,
+                                 uint32_t &meshDieId,
+                                 std::map<u32, std::vector<HcclChannelDesc>> &rankIdToChannelDesc);
+
     uint32_t mySubCommRank_ = 0;
+    std::map<uint32_t, std::vector<HcclChannelDesc>> channels_;   // key is DieId
+    std::map<uint32_t, std::vector<u32>> rankGroup_;              // key is DieId
+    std::map<u32, std::vector<HcclChannelDesc>> rankIdToChannelDesc_;
 };
 
 }// namespace ops_hccl
