e25b465ea9708bf389523a0d9de1065eee2fd798
gcw_dFpssWuk <wanghaixu5@h-partners.com>
2026-07-16 10:06:40 +0800

    fix ccu sched guard log: 3 level topo instead of level2Uboe

--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -106,7 +106,7 @@ SelectorStatus ReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNetLa
     CHK_PRT_RET(topoInfo == nullptr, HCCL_ERROR("[Algo][ReduceAutoSelector] topoInfo is nullptr"),
         SelectorStatus::NOT_MATCH);
     if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3) {
-        HCCL_ERROR("[ReduceAutoSelector][%s] ccu schedule is not supported with level2Uboe, reset to default.",
+        HCCL_ERROR("[ReduceAutoSelector][%s] ccu schedule is not supported with 3 level topo, reset to default.",
             __func__);
         return SelectorStatus::NOT_MATCH;
     }
--- src/ops/scatter/selector/scatter_auto_selector.cc
+++ src/ops/scatter/selector/scatter_auto_selector.cc
@@ -36,7 +36,7 @@ SelectorStatus ScatterAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNetL
     HCCL_DEBUG("[ScatterAutoSelector][%s] start, topoInfo levelNum[%u]", __func__, topoInfo->topoLevelNums);
 
     if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3) {
-        HCCL_ERROR("[ScatterAutoSelector][%s] ccu schedule is not supported with level2Uboe, reset to default.",
+        HCCL_ERROR("[ScatterAutoSelector][%s] ccu schedule is not supported with 3 level topo, reset to default.",
             __func__);
         return SelectorStatus::NOT_MATCH;
     }
