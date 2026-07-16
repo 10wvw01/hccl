37ac3c195cc2929e894845198034fc41e2d45aba
wangyujiang98 <wangyujiang3@h-partners.com>
2026-07-16 16:20:51 +0800

    HCCL order Launch

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -274,6 +274,15 @@ extern "C" unsigned int HcclLaunchAicpuKernel(OpParam *param)
         return 1;
     }
 
+    // 获取host侧保序流、device侧保序流
+    ThreadHandle exportHostOrderThread;
+    exportHostOrderThread = param->exportHostOrderThread;
+    ThreadHandle deviceOrderThread;
+    deviceOrderThread = param->deviceOrderThread;
+    // notify record
+    CHK_RET(static_cast<HcclResult>(HcommThreadNotifyRecordOnThread(deviceOrderThread, exportHostOrderThread, 
+        HOST_ORDER_THREAD_NOTIFY_IDX)));
+
     std::string algName = std::string(param->algName);
     if (!ops_hccl::IsOpsV2(param->algName, param->deviceType)) {
         ScatterOpInfo opInfo;
