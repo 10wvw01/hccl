ab66f29b327d947b7d42bd92a4b2867987533d9c
luomin2005 <icecool.luomin@huawei.com>
2026-07-16 18:01:21 +0800

    add parallel testcase

--- refactor/ops/executor/ops_executor.cc
+++ refactor/ops/executor/ops_executor.cc
@@ -454,7 +454,7 @@ inline void OpsExecutor::MergeChildrenOutput(const AlgoExecDesc &algoExecDesc,
 
 HcclResult OpsExecutor::RunTemplateDesc(TemplateExecDesc *templateExeDes, AlgoExecDataDesc &algoExecDataDesc)
 {
-    HCCL_INFO("[RunTemplateDesc] templateExeDes: hcclCmdType=%d, algType=%d, subCommIndex=%d",
+    HCCL_ERROR("[RunTemplateDesc] templateExeDes: hcclCmdType=%d, algType=%d, subCommIndex=%d",
         static_cast<int>(templateExeDes->templateDesc.hcclCmdType),
         static_cast<int>(templateExeDes->templateDesc.algType), templateExeDes->subCommIndex);
     std::vector<u32> templateRanks = algHierarchyInfo_.infos[templateExeDes->subCommIndex].at(0);
@@ -465,7 +465,7 @@ HcclResult OpsExecutor::RunTemplateDesc(TemplateExecDesc *templateExeDes, AlgoEx
     // 根据阶段生成template的数据参数
     TemplateDataParams templateDataParams;
     GenTemplateDataParams(algoExecDataDesc, templateDataParams);
-    HCCL_INFO("[RunTemplateDesc] templateDataParams: inputBufferType=%d, outputBufferType=%d, cclBufferType=%d, "
+    HCCL_ERROR("[RunTemplateDesc] templateDataParams: inputBufferType=%d, outputBufferType=%d, cclBufferType=%d, "
               "dataType=%d, dataOffset=%lu, sliceCount=%lu, sliceOffset=%lu, tailCount=%lu, dataStride=%lu, "
               "scratchStride=%lu, reduceOp=%d, root=%u, enableRemoteMemAccess=%d",
         static_cast<int>(templateDataParams.inputBufferType), static_cast<int>(templateDataParams.outputBufferType),
