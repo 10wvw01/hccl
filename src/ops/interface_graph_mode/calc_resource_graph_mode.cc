ac7caa819364d02933723d5a407dca401da428b9
shirokawa_ <jinliuyi@huawei.com>
2026-07-17 10:02:09 +0800

    add

--- src/ops/interface_graph_mode/calc_resource_graph_mode.cc
+++ src/ops/interface_graph_mode/calc_resource_graph_mode.cc
@@ -425,7 +425,7 @@ HcclResult HcclGetAlgExecParamGraphMode(const char *tag, const char *group, u64 
     superKernelArgs.dataType = dataType;
     superKernelArgs.unitSize = ops_hccl::DATATYPE_SIZE_TABLE[dataType];
     superKernelArgs.reduceOp = op;
-    superKernelArgs.numBlocks = aivCoreLimit;
+    superKernelArgs.numBlocks = 32;
     superKernelArgs.tag = 0;
     superKernelArgs.clearEnable = clearEnable;
     superKernelArgs.inputSliceStride = 0;
