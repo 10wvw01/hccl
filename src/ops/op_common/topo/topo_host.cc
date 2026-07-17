b2eb57f30bff7cf7f261caaabbab6bc45ad4f7da
ooooo-create <fangguoyong@h-partners.com>
2026-07-17 13:33:33 +0800

    Adapt asc-comm primitives header include path

--- src/ops/op_common/topo/topo_host.cc
+++ src/ops/op_common/topo/topo_host.cc
@@ -12,7 +12,7 @@
 #include <vector>
 #include "topo_host.h"
 #include "hccl_rank_graph.h"
-#include "hcomm_primitives.h"
+#include "aicpu/hcomm/hcomm_primitives.h"
 #include "hccl_res.h"
 #include "hccl.h"
 #include "adapter_acl.h"
