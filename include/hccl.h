737f196f0a4c695f2256f237eaafd8e0c79439c8
TheoCui <2381845510@qq.com>
2026-07-19 20:15:46 +0800

    fix: 补充 HcclReduceScatterV 注释中遗漏的 prod 归约类型
    关联Issue: #309
    
    include/hccl.h 第80行 HcclReduceScatterV 的 @param op 注释
    只列出 sum, max, min，遗漏了 Ascend 950 支持的 prod 类型。
    与 API 文档 docs/zh/api_ref/comm_op_interface/HcclReduceScatterV.md
    保持一致。

--- include/hccl.h
+++ include/hccl.h
@@ -77,7 +77,7 @@ extern HcclResult HcclReduceScatter(void *sendBuf, void *recvBuf, uint64_t recvC
  * @param recvCount An integer(u64) identifying the number of the output data.
  * @param dataType The data type of the operator, must be one of the following types: int8, int16, int32, int64,
  * float16, float32, bfp16.
- * @param op The reduction type of the operator, must be one of the following types: sum, max, min.
+ * @param op The reduction type of the operator, must be one of the following types: sum, prod, max, min.
  * @param comm A pointer identifying the communication resource based on.
  * @param stream A pointer identifying the stream information.
  * @return HcclResult
