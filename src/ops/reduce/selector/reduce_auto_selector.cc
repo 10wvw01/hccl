16bfba2aaf99df516537c093181bba51dc41ce4f
weixin_43960572 <yuantangzhi@huawei.com>
2026-07-21 12:00:41 +0800

    fix broadcast and reduce selector

--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -133,8 +133,8 @@ SelectorStatus ReduceAutoSelector::SelectCcuScheduleAlgo(const TopoInfoWithNetLa
 
     if (topoInfo->topoLevelNums > 1) {
         if (topoInfo->level0Topo == Level0Shape::MESH_1D) {
-            if (topoInfo->userRankSize == 0 ||
-                dataSize / topoInfo->userRankSize > CCU_SCHEDULE_2LEVEL_MAX_PER_RANK_DATA_SIZE) {
+            if ((topoInfo->userRankSize == 0 ||
+                dataSize / topoInfo->userRankSize > CCU_SCHEDULE_2LEVEL_MAX_PER_RANK_DATA_SIZE) && topoInfo->userRankSize > 32) {
                 HCCL_INFO("[ReduceAutoSelector] 2 level topo perRankDataSize[%llu] exceeds limit, fallback to aicpu.",
                     topoInfo->userRankSize == 0 ? dataSize : dataSize / topoInfo->userRankSize);
                 return SelectorStatus::NOT_MATCH;
