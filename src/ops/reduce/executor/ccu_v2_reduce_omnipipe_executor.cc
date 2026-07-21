f3d36d2c1ed43a0e14aad3289e472e9e9d0c7dc2
unknown <zhangqian305@h-partners.com>
2026-07-21 19:35:11 +0800

    rs_y

--- src/ops/reduce/executor/ccu_v2_reduce_omnipipe_executor.cc
+++ src/ops/reduce/executor/ccu_v2_reduce_omnipipe_executor.cc
@@ -638,7 +638,7 @@ HcclResult CcuV2ReduceOmniPipeExecutor<AlgTopoMatch, CcuRsAlgTemplateX, CcuRsAlg
 
             for (u32 i = 0; i < rankSize_; i++) {
                 if (loop != 0) {
-                     processedDataCountTmp[i] = processedDataCountTmp[i] + multiLoopAllRankSplitData[loop-1][i];
+                    processedDataCountTmp[i] = processedDataCountTmp[i] + multiLoopAllRankSplitData[loop-1][i];
                     HCCL_DEBUG("processedDataCountTmp[%lu]:[%lu] multiloop[%lu][%lu]:[%lu] ",i, processedDataCountTmp[i], loop, i, multiLoopAllRankSplitData[loop][i]);
                 }
             }
@@ -666,12 +666,19 @@ HcclResult CcuV2ReduceOmniPipeExecutor<AlgTopoMatch, CcuRsAlgTemplateX, CcuRsAlg
                 tempAlgParamLocalCopy.buffInfo.outBuffType = BufferType::OUTPUT;
                 tempAlgParamLocalCopy.buffInfo.outBuffBaseOff = rankOffset + processedDataCountTmp[i] * dataTypeSize_; // i * 512
                 tempAlgParamLocalCopy.stepSliceInfo.buffInfo.outBuffBaseOff = rankOffset + processedDataCountTmp[i] * dataTypeSize_;
-
                 if (i == param.root) {
                     tempAlgParamLocalCopy.buffInfo.inputPtr = param.inputPtr;
                     tempAlgParamLocalCopy.buffInfo.inBuffType = BufferType::INPUT;
                     tempAlgParamLocalCopy.buffInfo.inBuffBaseOff = rankOffset + processedDataCountTmp[i] * dataTypeSize_;
                     tempAlgParamLocalCopy.stepSliceInfo.buffInfo.inBuffBaseOff = rankOffset + processedDataCountTmp[i] * dataTypeSize_;
+                    if (loop == 0) {
+                        tempAlgParamLocalCopy.count = allRankSplitData[i];
+                        tempAlgParamLocalCopy.sliceSize = allRankSplitData[i] * dataTypeSize_;
+                    } else {
+                        rankOffset += allRankSplitData[i] * dataTypeSize_; // 卡偏移
+                        rankLoopOffset += multiLoopAllRankSplitData[loop][i] * dataTypeSize_;// 0 11 
+                       continue; 
+                    }
                 } else {
                     tempAlgParamLocalCopy.buffInfo.inputPtr = resCtx.cclMem.addr;
                     tempAlgParamLocalCopy.buffInfo.inBuffType = BufferType::HCCL_BUFFER;
@@ -681,7 +688,12 @@ HcclResult CcuV2ReduceOmniPipeExecutor<AlgTopoMatch, CcuRsAlgTemplateX, CcuRsAlg
 
                 HCCL_DEBUG("[%s] myRank[%u]  inBuffBaseOff[%lu] outBuffBaseOff[%lu] sliceSize[%lu] processedDataCount[%lu] rankOffset[%lu] rankLoopOffset[%lu]", __func__,
                 myRank_, tempAlgParamLocalCopy.buffInfo.inBuffBaseOff, tempAlgParamLocalCopy.buffInfo.outBuffBaseOff, tempAlgParamLocalCopy.sliceSize, processedDataCount, rankOffset, rankLoopOffset);
-                CHK_RET(gAlgTempX.KernelRun(param, tempAlgParamLocalCopy, templateResourceGX));
+                if (i % 2 == 0) {
+                    CHK_RET(gAlgTempX.KernelRun(param, tempAlgParamLocalCopy, templateResourceGX));
+                } else {
+                    CHK_RET(gAlgTempY.KernelRun(param, tempAlgParamLocalCopy, templateResourceGY));
+                }
+               
                 rankOffset += allRankSplitData[i] * dataTypeSize_; // 卡偏移
                 rankLoopOffset += multiLoopAllRankSplitData[loop][i] * dataTypeSize_;// 0 11 11*2 11*3 11*4 11*4+9 11*5+9
             }
@@ -692,7 +704,7 @@ HcclResult CcuV2ReduceOmniPipeExecutor<AlgTopoMatch, CcuRsAlgTemplateX, CcuRsAlg
         processedDataCount += maxCountPerLoop;
     }
 
-    HCCL_INFO("[%s][OrchestrateLoop] Endxx.", __func__);
+    HCCL_INFO("[%s][OrchestrateLoop] Endzq.", __func__);
     return HCCL_SUCCESS;
 }
 
