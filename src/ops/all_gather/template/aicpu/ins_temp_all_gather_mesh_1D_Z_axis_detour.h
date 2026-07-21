4587857fa3808cae5f80104ec8118ec893ff5de8
cokelin <linguoxiong4@huawei.com>
2026-07-21 11:12:45 +0800

    checker通过的版本，z轴绕路类多加两函数

--- src/ops/all_gather/template/aicpu/ins_temp_all_gather_mesh_1D_Z_axis_detour.h
+++ src/ops/all_gather/template/aicpu/ins_temp_all_gather_mesh_1D_Z_axis_detour.h
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
- * CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #ifndef INS_TEMP_ALL_GATHER_MESH_1D_Z_AXIS_DETOUR_H
 #define INS_TEMP_ALL_GATHER_MESH_1D_Z_AXIS_DETOUR_H
@@ -31,6 +31,13 @@ public:
                                         std::vector<u64> &elemCountOut, std::vector<u64> &sizeOut,
                                         std::vector<u64> &elemOffset) override;
     HcclResult SetchannelsPerRank(const std::map<u32, std::vector<ChannelInfo>> &channels) override;
+
+    // Z轴绕路场景下，cclBuff由框间NHR按一维[rpt]布局写入，本rank无需再LocalDataCopy里做input->cclBuff的
+    // 自拷贝；且RunAllGatherMesh从remote cclBuff读取时，应按inputSliceStride(框间写入的步进)索引，
+    // 而非基类standalone用的sliceSize(基类ccl-copy建立的[rpt][algRank]二维布局步进)。
+    HcclResult LocalDataCopy(const std::vector<ThreadHandle> &threads) override;
+    HcclResult RunAllGatherMesh(const std::vector<ThreadHandle> &threads,
+                                const std::map<u32, std::vector<ChannelInfo>> &channels) override;
 protected:
     u32 level0ChannelNumPerRank_{1};
     u32 level1ChannelNumPerRank_{0};
