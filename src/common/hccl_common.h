bc7376fd2e8c3e5544430d7d918f909ab36e1cca
a1847373419 <zhengyuntao2@huawei.com>
2026-07-21 10:57:30 +0800

    refactor: 移除所有 Ascend_950_CCU_V2 宏定义及条件编译守卫

--- src/common/hccl_common.h
+++ src/common/hccl_common.h
@@ -23,7 +23,6 @@
 #include "hccl_types.h"
 #include "hccl_res.h"
 
-#define Ascend_950_CCU_V2
 
 #ifndef T_DESC
 #define T_DESC(_msg, _y) ((_y) ? true : false)
@@ -260,15 +259,11 @@ struct HcclMem {
 
 inline bool shouldGoOutPlace(DevType deviceType) {
 #ifdef MACRO_DEV_TYPE_NEW
-    #ifdef Ascend_950_CCU_V2
         #if CANN_VERSION_NUM >= 90100000
             return deviceType == DevType::DEV_TYPE_950 || deviceType == DevType::DEV_TYPE_960;
         #else
             return deviceType == DevType::DEV_TYPE_950;
         #endif
-    #else
-        return deviceType == DevType::DEV_TYPE_950;
-    #endif
 #else
     return deviceType == DevType::DEV_TYPE_910_95;
 #endif
