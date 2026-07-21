dee379a3202c3720d12fb0680b6f1b2716c243c4
gcw_TwqkoH55 <shengsiyuan1@huawei.com>
2026-07-21 14:30:20 +0800

    comment function trace

--- src/ops/op_common/template/aicpu/kernel_launch.cc
+++ src/ops/op_common/template/aicpu/kernel_launch.cc
@@ -326,7 +326,7 @@ inline HcclResult OpOrchestrate(OpParam *param, const AlgResourceCtxSerializable
 
 extern "C" unsigned int HcclLaunchAicpuKernelInternal(OpParam *param, const uint64_t opUnfoldIdx)
 {
-    FUNCTION_TRACE;
+    // FUNCTION_TRACE;
     
     // 修改当前进程的调度策略和优先级
     struct sched_param schedParam;
