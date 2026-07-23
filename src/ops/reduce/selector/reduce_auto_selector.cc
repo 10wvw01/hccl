67a51e3045cfbedf7a639b259cdc8a7bcbf68a3c
cokelin <linguoxiong4@huawei.com>
2026-07-23 17:02:19 +0800

    [reduce3级]删除reduce2级testcase，恢复三级selector

--- src/ops/reduce/selector/reduce_auto_selector.cc
+++ src/ops/reduce/selector/reduce_auto_selector.cc
@@ -236,12 +236,8 @@ SelectorStatus ReduceAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayerDet
             } else {
                 selectAlgName = "ReduceParallelNHRNHRUboe";
             }
-        // } else if (topoInfo->topoLevelNums == TOPO_LEVEL_3 && topoInfo->level0Topo == Level0Shape::MESH_1D) {
-        //     selectAlgName = "AicpuReduceSequenceMesh1DNHRNHR";
-        // 这块删除注意需要吧top0level2也删除
-        } else if ((topoInfo->topoLevelNums == 2 || topoInfo->topoLevelNums == TOPO_LEVEL_3) &&
-            topoInfo->level0Topo == Level0Shape::MESH_1D) {
-            selectAlgName = "AicpuReduceSequenceMesh1DNHR";
+        } else if (topoInfo->topoLevelNums == TOPO_LEVEL_3 && topoInfo->level0Topo == Level0Shape::MESH_1D) {
+            selectAlgName = "AicpuReduceSequenceMesh1DNHRNHR";
         } else if (topoInfo->Level1Nhr) {
             selectAlgName = "ReduceNHR";
         } else if (topoInfo->deviceNumPerModule > 1 && topoInfo->level0Topo == Level0Shape::MESH_1D) {
