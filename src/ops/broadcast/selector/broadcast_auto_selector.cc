4b113b977654761672ca4d6b0fbd1822109cf5ac
hanjf <hanjiafeng1@huawei.com>
2026-07-23 19:17:01 +0800

    2 level support

--- src/ops/broadcast/selector/broadcast_auto_selector.cc
+++ src/ops/broadcast/selector/broadcast_auto_selector.cc
@@ -179,8 +179,10 @@ SelectorStatus BroadcastAutoSelector::SelectAicpuAlgo(const TopoInfoWithNetLayer
         if (topoInfo->topoLevelNums == TOPO_LEVEL_NUM_3) {
             if (topoInfo->netLayerDetails.localNetInsSizeOfLayer[1] == 1) {
                 selectAlgName = "InsBroadcastNHR";
+            } else if (topoInfo->level0Topo == Level0Shape::MESH_1D && !topoInfo->level2Uboe) {
+                selectAlgName = "AicpuBroadcastSequenceMesh1dNHRNHR";
             } else {
-                selectAlgName = "InsBroadcastSequenceMesh1DNHRNHR";
+                selectAlgName = "InsBroadcastParallelNHRNHRUboe";
             }
         } else if (topoInfo->Level1Nhr) {
             selectAlgName = "InsBroadcastNHR";
