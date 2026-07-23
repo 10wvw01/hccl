1733184f8f9b2d44fe79aa1fae09e76053f0ed1c
wjl <jellywang2000@gmail.com>
2026-07-23 14:50:12 +0800

    fastlaunch

--- src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h
+++ src/ops/reduce_scatter/template/ccu/ccu_temp_reduce_scatter_mesh_1D_2die_mem2mem.h
@@ -38,6 +38,7 @@ public:
     HcclResult KernelRun(const OpParam& param,
                          const TemplateDataParams& templateDataParams,
                          TemplateResource& templateResource) override;
+    HcclResult FastLaunch(const OpParam& param, const TemplateFastLaunchCtx& tempFastLaunchCtx) override;              
     u64 GetThreadNum() const override;
     u64 CalcScratchMultiple(BufferType inBuffType, BufferType outBuffType) override;
 
