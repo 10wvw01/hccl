37c99e265c1acad639a601686c42d95b9de3ed75
gcw_TwqkoH55 <shengsiyuan1@huawei.com>
2026-07-23 14:43:54 +0800

    update OpParam

--- src/ops/op_common/inc/alg_param.h
+++ src/ops/op_common/inc/alg_param.h
@@ -606,12 +606,10 @@ struct OpParam { // 不申请ctx，每个算子单独下发
     u32 aicpuRecordCpuIdx = 0; // aicpu record host的notifyIdx
     u32 dataCount = 0; // 算子上报dfx的数据量
     DevAicpuOpConfig opConfig; // 收编算子配置类变量
+    bool aicpuCacheEnable = false; // aicpu task cache开关
+    bool isCapture = false; // 是否为aclgraph
     u64 varMemSize{0};
     u8 varData[0];
-
-    // aicpu task cache新增字段
-    bool aicpuCacheEnable = false;
-    bool isCapture = false;
 };
 
 struct AlgDesc {
