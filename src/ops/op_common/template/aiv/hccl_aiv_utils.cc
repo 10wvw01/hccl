5e2170f996cb7ffb8159e02193c8e297d1aa279f
chenyia <chenyi163@huawei.com>
2026-07-24 10:26:39 +0800

    aiv alltoallv cache

--- src/ops/op_common/template/aiv/hccl_aiv_utils.cc
+++ src/ops/op_common/template/aiv/hccl_aiv_utils.cc
@@ -846,6 +846,36 @@ HcclResult ReplayAivInstructions(const AivInstruction *instructions, u32 insCoun
     return HCCL_SUCCESS;
 }
 
+HcclResult ReplayAivInstructionsV(const AivInstruction *instructions, u32 insCount, OpParam &param)
+{
+    if (insCount == 0) {
+        HCCL_WARNING("[ReplayAivInstructionsV] insCount is 0, skip replay.");
+        return HCCL_SUCCESS;
+    }
+    const u64 *sendCounts = static_cast<const u64 *>(param.all2AllVDataDes.sendCounts);
+    const u64 *sdispls = static_cast<const u64 *>(param.all2AllVDataDes.sdispls);
+    const u64 *recvCounts = static_cast<const u64 *>(param.all2AllVDataDes.recvCounts);
+    const u64 *rdispls = static_cast<const u64 *>(param.all2AllVDataDes.rdispls);
+    u32 rankSize = instructions[0].opArgs.rankSize;
+
+    for (uint32_t i = 0; i < insCount; ++i) {
+        AivOpArgs newArgs = instructions[i].opArgs;
+        newArgs.stream = param.stream;
+        newArgs.input = reinterpret_cast<u64>(param.inputPtr) + instructions[i].inputOffset;
+        newArgs.output = reinterpret_cast<u64>(param.outputPtr) + instructions[i].outputOffset;
+
+        for (u32 j = 0; j < rankSize; ++j) {
+            newArgs.extraArgs.sendCounts[j] = sendCounts[j];
+            newArgs.extraArgs.sendDispls[j] = sdispls[j];
+            newArgs.extraArgs.recvCounts[j] = recvCounts[j];
+            newArgs.extraArgs.recvDispls[j] = rdispls[j];
+        }
+
+        CHK_RET(ExecuteKernelLaunch(newArgs));
+    }
+    return HCCL_SUCCESS;
+}
+
 HcclResult StoreAivCacheCtx(HcclComm comm, const std::string &ctxTag, u64 keyHash, const std::string &algName,
                             AivCacheIndexCtx *indexCtx)
 {
